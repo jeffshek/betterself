a41a4b9359e7921c41702a0f3c35d414594a90dd
jeffshek <jeffshek@gmail.com>
2016-05-12 20:28:42 -0400

    Move over original correlation script

---
+++ tests/test_betterself.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_betterself
+----------------------------------
+
+Tests for `betterself` module.
+"""
+
+import unittest
+
+from betterself import betterself
+
+
+class TestBetterself(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
