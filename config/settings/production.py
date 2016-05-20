97d5cc9e8f74eb6fe314f6ec1d68a7de630f0b17
jeffshek <jeffshek@gmail.com>
2016-05-20 15:12:31 -0400

    allow herokuapp domain while dealing with dns issues

--- config/settings/production.py
+++ config/settings/production.py
@@ -80,7 +80,10 @@ SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT', default=True)
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['betterself.io'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[
+    'betterself.io',
+    'betterself.herokuapp.com']
+)
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ('gunicorn', )
