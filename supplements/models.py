07556d5cb011c16bc0d73f2e2dca753481b390a4
jeffshek <jeffshek@gmail.com>
2016-05-24 08:03:43 -0400

    add supplement product and ingredient comp

--- supplements/models.py
+++ supplements/models.py
@@ -10,4 +10,28 @@ class Ingredient(BaseTimeModel):
     half_life_minutes = models.PositiveIntegerField(null=True, blank=True)
 
 
+class MeasurementUnit(BaseTimeModel):
+    name = models.CharField(max_length=100)  # 'milligram'
+    short_name = models.CharField(max_length=100)  # 'ml'
+    is_liquid = models.BooleanField(default=False)
 
+
+class IngredientComposition(BaseTimeModel):
+    """
+    Creatine, 5, grams
+    """
+    ingredient = models.ManyToManyField(Ingredient)
+    measurement_unit = models.ForeignKey(MeasurementUnit)
+    quantity = models.FloatField(default=1)
+
+
+class SupplementProduct(BaseTimeModel):
+    """
+    Could be a stack like BCAA (which would have 4 ingredient comps)
+    Or could just be something simple like Caffeine.
+    """
+    ingredient_composition = models.ForeignKey(IngredientComposition)
+    user_generated = models.BooleanField(default=False)
+    # company = models.ForeignKey()
+    # is this the best place to denote user derived products?
+    # user = models.ForeignKey()
