fa3d710c8c303dbe930809be0ea7e094c932c49d
jeffshek <jeffshek@gmail.com>
2016-05-30 12:57:52 -0400

    add basic shell of test in events to prevent same issue again

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class EventsModelsTests(TestCase):
+    def test_all_models_in_test_have_required_users(self):
+        # made this mistake where user wasn't required when first making event models ... realized
+        # what good is an event if it isn't tied to a user?
+        pass
