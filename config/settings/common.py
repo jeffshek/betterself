22ad8475576738a64d7c3f9afb44d8024f1aed94
jeffshek <jeffshek@gmail.com>
2016-07-02 13:23:00 -0400

    clean up file path structure in settings

--- config/settings/common.py
+++ config/settings/common.py
@@ -12,6 +12,7 @@ from __future__ import absolute_import, unicode_literals
 
 import environ
 
+# return where ever the project starts at
 ROOT_DIR = environ.Path(__file__) - 3  # (/a/b/myfile.py - 3 = /)
 APPS_DIR = ROOT_DIR.path('betterself')
 
--- importers/tests.py
+++ importers/tests.py
@@ -1,8 +1,7 @@
 import pandas as pd
-
-from django.test import TestCase
+from django.conf import settings
 from django.contrib.auth import get_user_model
-
+from django.test import TestCase
 from importers.excel.importer import SupplementSanitizerTemplate
 
 # use django user model
@@ -11,13 +10,14 @@ User = get_user_model()
 
 class ExcelImporterTests(TestCase):
     def setUp(self):
-
         # create user
         User.objects.create_user(username='jacob', email='jacob@donkey.com', password='top_secret')
         self.user = User.objects.get(username='jacob')
 
     def test_excel_importer(self):
-        file_path = '/sites/betterself/importers/fixtures/tests/supplement_log_fixtures.xlsx'
+        file_path = settings.ROOT_DIR.path("importers", "fixtures", "tests", "supplement_log_fixtures.xlsx")
+        # path is like a command pattern, so we call it now to get the string location
+        file_path = file_path()
 
         sanitizer = SupplementSanitizerTemplate(file_path, self.user)
         results = sanitizer.get_sanitized_dataframe()
@@ -37,10 +37,10 @@ class ExcelImporterTests(TestCase):
         self.assertEqual(result['quantity'], None)
 
     def test_columns_with_spaces_in_excel(self):
-        series = pd.Series([1,2,3])
+        series = pd.Series([1, 2, 3])
         dataframe = pd.DataFrame(
             {
-                '               A': series,   # make some ridiculous columns
+                '               A': series,  # make some ridiculous columns
                 '               B (200mg)': series,
             }
         )
