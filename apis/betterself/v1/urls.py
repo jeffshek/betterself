9dff5e6915626c2f2059363bea4600b5c3c9c0bf
jeffshek <jeffshek@gmail.com>
2016-07-16 13:55:33 -0400

    pre-commits help better processes

--- apis/betterself/v1/urls.py
+++ apis/betterself/v1/urls.py
@@ -2,6 +2,6 @@ from apis.betterself.v1.views import APIv1Views
 from django.conf.urls import url
 
 urlpatterns = [
-    # urls that contain all the resources
+    # urls should contain all rest resources
     url(r'^supplements', APIv1Views.as_view()),
 ]
