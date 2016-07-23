8f1d86ecb03cb7493bb4e1077063adf496435bbf
jeffshek <jeffshek@gmail.com>
2016-07-23 16:46:45 -0400

    import sorter ignores django vs app level import order removing it

--- apis/betterself/v1/urls.py
+++ apis/betterself/v1/urls.py
@@ -1,5 +1,6 @@
-from apis.betterself.v1.views import SupplementProductView
 from django.conf.urls import url
+
+from apis.betterself.v1.views import SupplementProductView
 
 urlpatterns = [
     # urls should contain all rest resources
