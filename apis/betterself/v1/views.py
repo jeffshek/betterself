3eb1e4616ecb9d10fe547e7f28b439acc6a4b095
jeffshek <jeffshek@gmail.com>
2016-07-24 18:47:10 -0400

    extend generics with a queryset filter

--- apis/betterself/v1/views.py
+++ apis/betterself/v1/views.py
@@ -6,26 +6,28 @@ from apis.betterself.v1.serializers import IngredientCompositionSerializer, Supp
 from supplements.models import Ingredient, IngredientComposition, Measurement, Supplement
 
 
-class BaseGenericAPIViewV1(GenericAPIView):
-    model = None
-
-    def get_queryset(self):
-        # for all objects returned, a user should only see either
-        # objects that don't belong to a user or objects owned by a
-        # specific user
-        queryset = self.model.filter(Q(user=self.request.user) | Q(user=None))
-        return queryset
-
-
-class BaseGenericListAPIViewV1(ListAPIView):
-    model = None
-
-    def get_queryset(self):
+class UserQuerysetFilterMixin(object):
+    # this is kind of weird, but because most of the resources need to have
+    # a ownership principle (access to objects with no owner or owned by specific user)
+    # have a mixin that will filter everything - hopefully preventing situations
+    # where someone has access to data they shouldn't
+    # ignore MRO / duped issues by using a template style
+    def _get_queryset(self):
         # for all objects returned, a user should only see either
         # objects that don't belong to a user or objects owned by a
         # specific user
         queryset = self.model.objects.filter(Q(user=self.request.user) | Q(user=None))
         return queryset
+
+
+class BaseGenericAPIViewV1(GenericAPIView, UserQuerysetFilterMixin):
+    def get_queryset(self):
+        return self._get_queryset()
+
+
+class BaseGenericListAPIViewV1(ListAPIView, UserQuerysetFilterMixin):
+    def get_queryset(self):
+        return self._get_queryset()
 
 
 class IngredientView(BaseGenericAPIViewV1):
