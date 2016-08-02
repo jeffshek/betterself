44d09bed3a7c5103f7b942f38ee9124806c2eaff
Jeffrey Shek <jeffshek@gmail.com>
2016-08-02 11:02:57 +0000

    Create composition linking if vendor_id is passed

--- apis/betterself/v1/serializers.py
+++ apis/betterself/v1/serializers.py
@@ -63,10 +63,44 @@ class SupplementCreateSerializer(serializers.Serializer):
 
         vendor_id = validated_data.pop('vendor_id', None)
         if vendor_id:
-            vendor = Vendor.objects.filter(id=vendor_id).filter(Q(user=user) | Q(user__isnull=True))
-            if not vendor:
-                raise ValidationError('No Vendor ID of {0} found belong to user {1}'.format(vendor_id, user.id))
-            else:
-                validated_data['vendor'] = vendor.first()
+            validated_data = self._add_vendor_to_validated_data(user, validated_data, vendor_id)
 
-        return Supplement(**validated_data)
+        ingredient_compositions = []
+        ingredient_compositions_ids = validated_data.pop('ingredient_compositions_ids', None)
+        if ingredient_compositions_ids:
+            ingredient_compositions = self._get_ingredient_compositions_accessible_for_user(user,
+                ingredient_compositions_ids)
+
+        # cannot associate many to many unless item has been saved
+        supplement = Supplement.objects.create(**validated_data)
+
+        for composition in ingredient_compositions:
+            supplement.ingredient_compositions.add(composition)
+
+        return supplement
+
+    @staticmethod
+    def _get_ingredient_compositions_accessible_for_user(user, ingredient_compositions_ids):
+        try:
+            ingredient_compositions_ids_parsed = [int(item) for item in ingredient_compositions_ids.strip().split(',')]
+        except TypeError:
+            raise TypeError('Invalid Ingredient Compositions Ids Entered {0}'.format(ingredient_compositions_ids))
+
+        queryset = IngredientComposition.objects.filter(id__in=ingredient_compositions_ids_parsed).filter(
+            Q(user=user) | Q(user__isnull=True))
+
+        if not queryset.first():
+            raise ValidationError('No Ingredient Composition IDs of {0} found belong to user {1}'.format(
+                ingredient_compositions_ids, user.id))
+        else:
+            return queryset
+
+    @staticmethod
+    def _add_vendor_to_validated_data(user, validated_data, vendor_id):
+        vendor = Vendor.objects.filter(id=vendor_id).filter(Q(user=user) | Q(user__isnull=True))
+        if not vendor:
+            raise ValidationError('No Vendor ID of {0} found belong to user {1}'.format(vendor_id, user.id))
+        else:
+            validated_data['vendor'] = vendor.first()
+
+        return validated_data
