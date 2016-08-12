9883a1ac995816160a35fd66107a576289062123
jeffshek <jeffshek@gmail.com>
2016-08-11 23:42:47 -0400

    Add queryset, but debate if better options

--- apis/betterself/v1/events/views.py
+++ apis/betterself/v1/events/views.py
@@ -6,3 +6,12 @@ from events.models import SupplementEvent
 class SupplementEventView(BaseGenericListCreateAPIViewV1):
     serializer_class = SupplementEventSerializer
     model = SupplementEvent
+
+    def get_queryset(self):
+        name = self.request.query_params.get('name')
+        if name:
+            queryset = self.model.objects.filter(name__iexact=name)
+        else:
+            queryset = self.model.objects.all()
+
+        return queryset
