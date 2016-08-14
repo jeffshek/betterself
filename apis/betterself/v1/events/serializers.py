4c5daca251158745403e8ce5948c1bee6e93ec01
Jeffrey Shek <jeffshek@gmail.com>
2016-08-14 23:57:15 +0000

    Found a better way to validate that I wasn't aware of

--- apis/betterself/v1/events/serializers.py
+++ apis/betterself/v1/events/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 
+from supplements.models import Supplement
+
 
 class SupplementEventSerializer(serializers.Serializer):
-    supplement_event_id = serializers.IntegerField(source='id')
     supplement_product_id = serializers.IntegerField()
     quantity = serializers.FloatField(default=1)
     time = serializers.DateTimeField()
+
+    def validate_supplement_product_id(self, value):
+        user = self.context['request'].user
+        user_viewable_supplements = Supplement.get_user_viewable_objects(user)
+        valid_supplement_ids = user_viewable_supplements.values_list('id', flat=True)
+        if value not in valid_supplement_ids:
+            raise serializers.ValidationError('Invalid Supplement Product Id')
 
     def create(self, validated_data):
         user = self.context['request'].user
--- apis/betterself/v1/events/tests.py
+++ apis/betterself/v1/events/tests.py
@@ -1,8 +1,12 @@
+import datetime
+import json
+
 from apis.betterself.v1.tests import BaseAPIv1Tests
 from apis.betterself.v1.urls import API_V1_LIST_CREATE_URL
 from events.fixtures.mixins import EventModelsFixturesGenerator
 from events.models import SupplementEvent
 from supplements.fixtures.mixins import SupplementModelsFixturesGenerator
+from supplements.models import Supplement
 from vendors.fixtures.mixins import VendorModelsFixturesGenerator
 
 
@@ -16,7 +20,44 @@ class TestSupplementEvents(BaseAPIv1Tests):
         VendorModelsFixturesGenerator.create_fixtures()
         EventModelsFixturesGenerator.create_fixtures(cls.user)
 
-    def test_event_get_request(self):
+    def test_event_valid_get_request(self):
         url = API_V1_LIST_CREATE_URL.format(self.TEST_MODEL.RESOURCE_NAME)
         request = self.client.get(url)
         self.assertEqual(request.status_code, 200)
+
+    def test_event_empty_post_request(self):
+        url = API_V1_LIST_CREATE_URL.format(self.TEST_MODEL.RESOURCE_NAME)
+        request = self.client.post(url)
+        self.assertEqual(request.status_code, 400)
+
+    def test_event_valid_post_request(self):
+        url = API_V1_LIST_CREATE_URL.format(self.TEST_MODEL.RESOURCE_NAME)
+        now = datetime.datetime.now()
+        now = now.isoformat()
+
+        # get the first viewable supplement
+        viewable_supplement_id = Supplement.get_user_viewable_objects(self.user).first().id
+
+        data = {
+            'supplement_product_id': viewable_supplement_id,
+            'time': now,
+        }
+
+        data = json.dumps(data)
+        request = self.client.post(url, data, content_type='application/json')
+        self.assertEqual(request.status_code, 201)
+
+    def test_event_invalid_supplement_post_request(self):
+        url = API_V1_LIST_CREATE_URL.format(self.TEST_MODEL.RESOURCE_NAME)
+        now = datetime.datetime.now()
+        now = now.isoformat()
+
+        # negative ids don't exist ... so this should fail
+        data = {
+            'supplement_product_id': -1,
+            'time': now,
+        }
+
+        data = json.dumps(data)
+        request = self.client.post(url, data, content_type='application/json')
+        self.assertEqual(request.status_code, 400)
