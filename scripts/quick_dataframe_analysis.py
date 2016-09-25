0d7242fb55349003383e955cef01bd105fa5b34e
jeffshek <jeffshek@gmail.com>
2016-09-24 22:58:40 -0400

    add quick script used for analysis

---
+++ scripts/quick_dataframe_analysis.py
@@ -0,0 +1,33 @@
+from importers.excel.importer import SupplementSanitizerTemplate
+
+personal_fixtures_file = '/betterself/personal_fixtures/supplement_fixtures.xlsx'
+
+sanitizer = SupplementSanitizerTemplate(personal_fixtures_file, user=None)
+dataframe = sanitizer.get_sanitized_dataframe()
+
+dataframe_cols = dataframe.columns
+
+ignore_columns = [
+    'Day',
+    'Notes',
+]
+
+pretty_columns = [item for item in dataframe_cols if item not in ignore_columns]
+dataframe = dataframe[pretty_columns]
+
+for col in pretty_columns:
+    shifted_col_name = 'Yesterday - {0}'.format(col)
+    series = dataframe[col]
+    shifted_series = series.shift(1)
+
+    dataframe[shifted_col_name] = shifted_series
+
+correlation_driver = 'Productivity Time (Minutes)'
+# correlation_driver = "Productivity Score"
+
+correlation_results = dataframe.corr()[correlation_driver]
+correlation_results_sorted = correlation_results.sort_values(inplace=False)
+
+yesterday_cols = [item for item in dataframe.columns if 'Yesterday' in item]
+yesterday_correlations = correlation_results_sorted[yesterday_cols]
+yesterday_correlations = yesterday_correlations.sort_values(inplace=False)
