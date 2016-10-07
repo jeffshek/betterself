ed5595338d899d6a192da83e927bbe0a72245518
jeffshek <jeffshek@gmail.com>
2016-10-07 09:35:59 -0400

    setup base structure for dataframes

--- analytics/events/analytics.py
+++ analytics/events/analytics.py
@@ -1,9 +1,16 @@
+import pandas as pd
+
+from django.core.exceptions import ValidationError
+
+VALID_CORRELATION_METHODS = ['pearson', 'spearman', 'kendall']
+
+
 class DataFrameEventsAnalyzer(object):
     """
     Takes a DataFrame and returns analytics on top of it.
     """
     def __init__(self, dataframe, ignore_columns=None, rest_day_column_name=None):
-
+        # certain columns might be just notes or useless information that can be ignored
         if ignore_columns:
             assert isinstance(ignore_columns, list)
             self.ignore_columns = ignore_columns
@@ -34,21 +41,64 @@ class DataFrameEventsAnalyzer(object):
 
         return dataframe
 
-    def get_correlation_for_measurement(self, measurement, lookback_correlation=None):
-        """ IE. What DataFrame effect are you trying to correlate this to? """
-        if lookback_correlation:
-            dataframe = self._add_yesterday_correlation_to_dataframe(self.dataframe)
-        else:
-            dataframe = self.dataframe.copy()
+    def get_correlation_for_measurement(self, measurement, add_yesterday_lag=False, method='pearson', min_periods=1):
+        """
+        IE. Measurement is the column name of what you're trying to improve / correlate
+        If add_yesterday_lag is True, take a look if yesterday's supplements impacted
+        Productivity / Sleep / etc. today.
+        """
+        self._validate_correlation_method(method)
+        # copy lets me be certain each result doesn't mess up state
+        dataframe = self.dataframe.copy()
 
-        # if productivity is marked as zero, that shouldn't be used to measure effectiveness
-        # since it's almost impossible to get a zero using RescueTime as a productive driver
-        # this might not be the best measurement ... perhaps serializer should just fill with np.NaN
-        # so when you get the data here you can make a more accurate decision
-        valid_days = dataframe[measurement] != 0
-        dataframe = dataframe[valid_days]
+        if add_yesterday_lag:
+            dataframe = self._add_yesterday_correlation_to_dataframe(dataframe)
 
-        correlation_results = dataframe.corr()[measurement]
+        dataframe = self._remove_invalid_measurement_days(dataframe)
+
+        correlation_results = dataframe.corr(method, min_periods)[measurement]
         correlation_results_sorted = correlation_results.sort_values(inplace=False)
 
         return correlation_results_sorted
+
+    def get_rolling_correlation_for_measurement(self, measurement, window, method='pearson', min_periods=1):
+        self._validate_correlation_method(method)
+        # copy lets me be certain each result doesn't mess up state
+        dataframe = self.dataframe.copy()
+
+        dataframe = self._remove_invalid_measurement_days(dataframe)
+        # kind of an annoying internal debate, but theoretically for pearson (i can't verify for all)
+        # the sum of a lookback, ie. let's say you took
+        # day (x-axis)  | caffeine  | theanine  | productive time
+        # 0             | 100mg     | 0 mg      | 20m
+        # 1             | 50        | 0 mg      | 10m
+        # 2             | 150       | 0 mg      | 25m
+        # 3             | 100       | 100 mg    | 60m
+        # the sum versus average over a rolling period SHOULD result in the sme correlation
+        # should probably check this with a test ...
+        dataframe = pd.rolling_sum(dataframe, window=window)
+
+        correlation_results = dataframe.corr(method, min_periods)[measurement]
+        correlation_results_sorted = correlation_results.sort_values(inplace=False)
+
+        return correlation_results_sorted
+
+    @staticmethod
+    def _remove_invalid_measurement_days(dataframe, measurement):
+        """
+        if productivity is marked as zero, that shouldn't be used to measure effectiveness
+        since it's almost impossible to get a zero using RescueTime as a productive driver
+        this might not be the best measurement ... perhaps serializer should just fill with np.NaN
+        so when you get the data here you can make a more accurate decision to scrap
+        """
+        valid_days = dataframe[measurement] != 0
+        dataframe = dataframe[valid_days]
+        return dataframe
+
+    @staticmethod
+    def _validate_correlation_method(method):
+        if not isinstance(method, str):
+            raise ValidationError('Correlation must be a string')
+
+        if method not in VALID_CORRELATION_METHODS:
+            raise ValidationError('Correlation must be one of {0} methods'.format(VALID_CORRELATION_METHODS))
---
+++ analytics/events/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+# python manage.py test analytics.events
+
+
+class EventsAnalyticsDataFrameTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super(EventsAnalyticsDataFrameTests, cls).setUpTestData()
+
+    def setUp(self):
+        pass
+
+    def test_analytics_dataframe(self):
+        pass
