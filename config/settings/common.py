06065d0a384ebac72e9fb557b9ffb9b56ffdb911
jeffshek <jeffshek@gmail.com>
2017-01-02 12:15:31 -0500

    switch extensions to base

--- config/settings/common.py
+++ config/settings/common.py
@@ -55,6 +55,8 @@ LOCAL_APPS = (
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
+INSTALLED_APPS += ('django_extensions', )
+
 # MIDDLEWARE CONFIGURATION
 # ------------------------------------------------------------------------------
 MIDDLEWARE_CLASSES = (
