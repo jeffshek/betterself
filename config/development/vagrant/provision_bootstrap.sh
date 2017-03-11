88494aba047569a8bc5d1b12c2a4cd649cb3bd68
jeffshek <jeffshek@gmail.com>
2017-03-10 19:03:52 -0500

    Add comment about how to use yarn

--- config/development/vagrant/provision_bootstrap.sh
+++ config/development/vagrant/provision_bootstrap.sh
@@ -13,6 +13,7 @@ apt-get libxslt1-dev -y
 apt-get install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python3.4 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 -y
 
 # load nvm to avoid a lot of headache ... add this to bashrc when you get time to refactor
+# switched to yarn, so this may / may not be necessary
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
