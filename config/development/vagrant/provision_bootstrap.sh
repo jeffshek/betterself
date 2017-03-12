733fc7e3b96a2fd64da74f3209c46cecbfbf605b
jeffshek <jeffshek@gmail.com>
2017-03-12 13:47:09 -0400

    Add some stuff for personal visualization

--- config/development/vagrant/provision_bootstrap.sh
+++ config/development/vagrant/provision_bootstrap.sh
@@ -12,6 +12,10 @@ apt-get install gcc -y
 apt-get libxslt1-dev -y
 apt-get install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python3.4 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 -y
 
+# to make graphviz work
+apt-get install graphviz -y
+apt-get install graphviz-dev -y
+
 # load nvm to avoid a lot of headache ... add this to bashrc when you get time to refactor
 # switched to yarn, so this may / may not be necessary
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
