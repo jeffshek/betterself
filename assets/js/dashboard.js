3a1a71b066008053f9ebe92b9244598b83996aec
jeffshek <jeffshek@gmail.com>
2017-05-04 20:28:51 -0400

    Redo CSS ... again

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -4,14 +4,8 @@ import Sidebar from "./fragments/sidebar"
 export class Dashboard extends Component {
   render() {
     return (
-      <div className="container body">
-        <div className="main_container">
-          <div className="col-md-3 left_col">
-            <div className="left_col scroll-view">
-              <Sidebar/>
-            </div>
-          </div>
-        </div>
+      <div>
+        <Sidebar/>
       </div>
     )
   }
