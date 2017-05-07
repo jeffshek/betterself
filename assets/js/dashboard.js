54389e9349aefc05e29704f01817d3c58e1e402b
jeffshek <jeffshek@gmail.com>
2017-05-06 23:16:49 -0400

    DEBT - So many different requirements its kind of embarassing

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Sidebar from "./sidebar/sidebar"
+import LoggedInHeader from "./header/internal_header"
 
 export class Dashboard extends Component {
   render() {
     return (
       <div>
+        <LoggedInHeader />
         <Sidebar/>
       </div>
     )
