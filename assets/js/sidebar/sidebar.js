cfeabf89b3e5273452c5212674a8239ffae9db9d
jeffshek <jeffshek@gmail.com>
2017-05-08 22:27:26 -0400

    Initial Mockup of Sidebar

--- assets/js/sidebar/sidebar.js
+++ assets/js/sidebar/sidebar.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Sidebar extends Component {
-
   handleClick(e) {
     e.preventDefault();
-    e.target.parentElement.classList.toggle('open');
+    e.target.parentElement.classList.toggle("open");
   }
 
   render() {
@@ -14,97 +13,134 @@ class Sidebar extends Component {
         <nav className="sidebar-nav">
           <ul className="nav">
             <li className="nav-item">
-              <Link to={'/dashboard'} className="nav-link">
+              <Link to={"/dashboard"} className="nav-link">
                 <i className="icon-speedometer" />
-                  Dashboard
+                Dashboard
                 <span className="badge badge-info">
                   NEW
                 </span>
               </Link>
             </li>
             <li className="nav-title">
-              UI Elements
+              Log
             </li>
-            <li className="nav-item nav-dropdown">
-               <a className="nav-link nav-dropdown-toggle" href="#" onClick={this.handleClick.bind(this)}><i className="icon-puzzle"></i> Components</a>
-               <ul className="nav-dropdown-items">
-                 <li className="nav-item">
-                   <Link to={'/components/buttons'} className="nav-link"><i className="icon-puzzle"></i> Buttons</Link>
-                 </li>
-                 <li className="nav-item">
-                   <Link to={'/components/social-buttons'} className="nav-link"><i className="icon-puzzle"></i> Social Buttons</Link>
-                 </li>
-                 <li className="nav-item">
-                   <Link to={'/components/cards'} className="nav-link"><i className="icon-puzzle"></i> Cards</Link>
-                 </li>
-                 <li className="nav-item">
-                   <Link to={'/components/modals'} className="nav-link"><i className="icon-puzzle"></i> Modals</Link>
-                 </li>
-                 <li className="nav-item">
-                   <Link to={'/components/switches'} className="nav-link"><i className="icon-puzzle"></i> Switches</Link>
-                 </li>
-                 <li className="nav-item">
-                   <Link to={'/components/tables'} className="nav-link"><i className="icon-puzzle"></i> Tables</Link>
-                 </li>
-                 <li className="nav-item">
-                   <Link to={'/components/tabs'} className="nav-link"><i className="icon-puzzle"></i> Tabs</Link>
-                 </li>
-               </ul>
-             </li>
-             <li className="nav-item nav-dropdown">
-               <a className="nav-link nav-dropdown-toggle" href="#" onClick={this.handleClick.bind(this)}><i className="icon-note"></i> Forms</a>
-               <ul className="nav-dropdown-items">
-                 <li className="nav-item">
-                   <Link to={'/forms/basic-forms'} className="nav-link"><i className="icon-note"></i> Basic Forms</Link>
-                 </li>
-                 <li className="nav-item">
-                   <Link to={'/forms/advanced-forms'} className="nav-link"><i className="icon-note"></i> Advanced Forms</Link>
-                 </li>
-               </ul>
-             </li>
 
             <li className="nav-item nav-dropdown">
-               <a className="nav-link nav-dropdown-toggle" href="#" onClick={this.handleClick.bind(this)}><i className="icon-energy"></i> Plugins</a>
-               <ul className="nav-dropdown-items">
-                 <li className="nav-item">
-                   <Link to={'/plugins/loading-buttons'} className="nav-link"><i className="icon-cursor"></i> Loading Buttons</Link>
-                 </li>
-                 <li className="nav-item">
-                   <Link to={'/plugins/spinners'} className="nav-link"><i className="fa fa-spinner"></i> Spinners</Link>
-                 </li>
-               </ul>
-             </li>
-             <li className="nav-item">
-               <Link to={'/widgets'} className="nav-link"><i className="icon-calculator"></i> Widgets <span className="badge badge-info">NEW</span></Link>
-             </li>
-             <li className="nav-item">
-               <Link to={'/charts'} className="nav-link"><i className="icon-pie-chart"></i> Charts</Link>
-             </li>
-             <li className="divider"></li>
-             <li className="nav-title">
-               Extras
-             </li>
-             <li className="nav-item nav-dropdown">
-               <a className="nav-link nav-dropdown-toggle" href="#" onClick={this.handleClick.bind(this)}><i className="icon-star"></i> Pages</a>
-               <ul className="nav-dropdown-items">
-                 <li className="nav-item">
-                   <Link to={'/pages/login'} className="nav-link"><i className="icon-star"></i> Login</Link>
-                 </li>
-                 <li className="nav-item">
-                   <Link to={'/pages/register'} className="nav-link"><i className="icon-star"></i> Register</Link>
-                 </li>
+              <a className="nav-link"><i className="icon-note" /> Events</a>
+            </li>
+
+            <li className="nav-item nav-dropdown">
+              <a
+                className="nav-link nav-dropdown-toggle"
+                href="#"
+                onClick={this.handleClick.bind(this)}
+              >
+                <i className="icon-puzzle" /> Fitness & Exercise
+              </a>
+              <ul className="nav-dropdown-items">
                 <li className="nav-item">
-                  <Link to={'/pages/404'} className="nav-link"><i className="icon-star"></i> Error 404</Link>
+                  <Link to={"/components/buttons"} className="nav-link">
+                    <i className="icon-puzzle" /> Cardio
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link to={'/pages/500'} className="nav-link"><i className="icon-star"></i> Error 500</Link>
+                  <Link to={"/components/social-buttons"} className="nav-link">
+                    <i className="icon-puzzle" /> Strength
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={"/components/cards"} className="nav-link">
+                    <i className="icon-puzzle" /> Heart Rate
+                  </Link>
                 </li>
               </ul>
-             </li>
+            </li>
+
+            <li className="nav-item nav-dropdown">
+              <a className="nav-link"><i className="icon-note" /> Heart Rate</a>
+              <a className="nav-link">
+                <i className="icon-note" /> Productivity
+              </a>
+              <a className="nav-link"><i className="icon-note" /> Sleep</a>
+              <a className="nav-link">
+                <i className="icon-note" /> Supplements
+              </a>
+
+            </li>
+            <li className="divider" />
+            <li className="nav-title">
+              Analytics
+            </li>
+
+            <li className="nav-item nav-dropdown">
+              <a
+                className="nav-link nav-dropdown-toggle"
+                href="#"
+                onClick={this.handleClick.bind(this)}
+              >
+                <i className="icon-energy" /> Health & Fitness
+              </a>
+              <ul className="nav-dropdown-items">
+                <li className="nav-item">
+                  <Link to={"/plugins/loading-buttons"} className="nav-link">
+                    <i className="icon-cursor" /> Strength
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={"/plugins/loading-buttons"} className="nav-link">
+                    <i className="icon-cursor" /> Heart Rate
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={"/plugins/loading-buttons"} className="nav-link">
+                    <i className="icon-cursor" /> Sleep
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <Link to={"/widgets"} className="nav-link">
+                <i className="icon-calculator" /> Productivity{" "}
+              </Link>
+            </li>
+
+            <li className="divider" />
+            <li className="nav-title">
+              Data Sources
+            </li>
+            <li className="nav-item nav-dropdown">
+              <a
+                className="nav-link nav-dropdown-toggle"
+                href="#"
+                onClick={this.handleClick.bind(this)}
+              >
+                <i className="icon-star" /> External
+              </a>
+              <ul className="nav-dropdown-items">
+                <Link to={"/plugins/loading-buttons"} className="nav-link">
+                  <i className="icon-cursor" /> Bose
+                </Link>
+                <Link to={"/plugins/loading-buttons"} className="nav-link">
+                  <i className="icon-cursor" /> FitBit
+                </Link>
+                <Link to={"/plugins/loading-buttons"} className="nav-link">
+                  <i className="icon-cursor" /> Garmin
+                </Link>
+                <Link to={"/plugins/loading-buttons"} className="nav-link">
+                  <i className="icon-cursor" /> GitHub
+                </Link>
+                <Link to={"/plugins/loading-buttons"} className="nav-link">
+                  <i className="icon-cursor" /> RescueTime
+                </Link>
+              </ul>
+              <li className="nav-item nav-dropdown">
+                <a className="nav-link"><i className="icon-note" /> Export</a>
+              </li>
+            </li>
           </ul>
         </nav>
       </div>
-    )
+    );
   }
 }
 
