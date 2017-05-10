8ec645eb75e82f464f3d75f81b3515f3ebceb098
jeffshek <jeffshek@gmail.com>
2017-05-10 00:05:25 -0400

    Switch to logo that has some cool shading

--- assets/js/fragments/image_paths.js
+++ assets/js/fragments/image_paths.js
@@ -4,7 +4,7 @@ import React, { PropTypes, Component } from "react";
 export const LOGO_BACKGROUND_PATH = require("../../../betterself/static/images/logos/logojoy/png/white_logo_transparent_background.png");
 export const FAVICON_PATH = require("../../../betterself/static/images/logos/logojoy/favicon.png");
 export const LOGIN_LEDGE_PHOTO_PATH = require("../../../betterself/static/images/login/login_image_ledge.jpeg");
-export const DASHBOARD_EXAMPLE_PATH = require("../../../betterself/static/images/dashboard/dashboard_example_3.png");
+export const DASHBOARD_EXAMPLE_PATH = require("../../../betterself/static/images/dashboard/dashboard_example_4.png");
 // HEADER_IMAGE_* eventually have an image that's a dropdown login/logout
 export const HEADER_BRAIN_PATH = require("../../../betterself/static/images/header/control_panel/brain.png");
 export const HEADER_HEART_PATH = require("../../../betterself/static/images/header/control_panel/heart.png");
