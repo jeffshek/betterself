b056f4baa04d4385c03b050f7f7327fef838fc7e
jeffshek <jeffshek@gmail.com>
2017-05-16 23:07:27 -0400

    Update URLs to not break

--- assets/js/urls/constants.jsx
+++ assets/js/urls/constants.jsx
@@ -1,12 +1,13 @@
 export const HOME_URL = '/'
 
-export const DASHBOARD_INDEX_URL = '/dashboard/'
+export const DASHBOARD_INDEX_URL = '/dashboard/index'
 // Logs
 export const DASHBOARD_EVENTS_LOGS_URL = '/dashboard/log/events'
 export const DASHBOARD_HEART_RATE_LOGS_URL = '/dashboard/log/heart_rates'
 export const DASHBOARD_PRODUCTIVITY_LOGS_URL = '/dashboard/logs/productivity'
 export const DASHBOARD_SLEEP_LOGS_URL = '/dashboard/logs/sleep'
 export const DASHBOARD_SUPPLEMENT_LOGS_URL = '/dashboard/log/supplements'
+// export const DASHBOARD_SUPPLEMENT_LOGS_URL = '/dashboard/log/events'
 // Analytics
 export const DASHBOARD_CHARTS_URL = '/dashboard/analytics/charts'
 
