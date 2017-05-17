e1425d59eae829db6e7cef42ba044fd218d13f8a
jeffshek <jeffshek@gmail.com>
2017-05-16 23:10:36 -0400

    Update URLs and rename Supplements

--- assets/js/urls/constants.jsx
+++ assets/js/urls/constants.jsx
@@ -1,13 +1,14 @@
 export const HOME_URL = '/'
 
 export const DASHBOARD_INDEX_URL = '/dashboard/index'
+
 // Logs
 export const DASHBOARD_EVENTS_LOGS_URL = '/dashboard/log/events'
 export const DASHBOARD_HEART_RATE_LOGS_URL = '/dashboard/log/heart_rates'
 export const DASHBOARD_PRODUCTIVITY_LOGS_URL = '/dashboard/logs/productivity'
 export const DASHBOARD_SLEEP_LOGS_URL = '/dashboard/logs/sleep'
 export const DASHBOARD_SUPPLEMENT_LOGS_URL = '/dashboard/log/supplements'
-// export const DASHBOARD_SUPPLEMENT_LOGS_URL = '/dashboard/log/events'
+
 // Analytics
 export const DASHBOARD_CHARTS_URL = '/dashboard/analytics/charts'
 
