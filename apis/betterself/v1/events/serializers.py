99ff6d91394dfa79588b74dfa43196919dd7aee0
jeffshek <jeffshek@gmail.com>
2017-05-18 21:51:05 -0400

    Thank god you wrote so many tests six months ago

--- apis/betterself/v1/events/serializers.py
+++ apis/betterself/v1/events/serializers.py
@@ -21,7 +21,7 @@ class SupplementEventCreateSerializer(serializers.Serializer):
     source = serializers.ChoiceField(INPUT_SOURCES_TUPLES)
     uuid = serializers.UUIDField(required=False, read_only=True)
     supplement_name = CharField(source='supplement.name', read_only=True, required=False)
-    duration = serializers.IntegerField()
+    duration = serializers.IntegerField(default=0)
 
     @classmethod
     def validate_supplement_uuid(cls, value):
