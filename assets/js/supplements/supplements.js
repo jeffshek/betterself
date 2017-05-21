8f1cd77945b9a907bc60d4d7397bafba8d7da276
jeffshek <jeffshek@gmail.com>
2017-05-21 16:11:17 -0400

    More mocking up of Supplements page

--- assets/js/supplements/supplements.js
+++ assets/js/supplements/supplements.js
@@ -79,61 +79,72 @@ class AddSupplementView extends Component {
           <strong>Create Supplement</strong> (Per Serving)
         </div>
         <div className="card-block">
-          <div className="row">
-            <div className="form-group col-sm-4">
-              <label><strong>Supplement Name</strong></label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Black Tea"
-              />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="form-group col-sm-4">
-              <label><strong>Vendor</strong></label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Lipton"
-              />
-
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="form-group col-sm-4">
-              <label><strong>Supplement Composition</strong></label>
-              <select className="form-control">
-                <option>Caffeine</option>
-              </select>
-              <select className="form-control">
-                <option>Theanine</option>
-              </select>
-            </div>
-            <div className="form-group col-sm-4">
-              <label><strong>Quantity</strong></label>
-              <select className="form-control">
-                <option>75</option>
-              </select>
-              <select className="form-control">
-                <option>150</option>
-              </select>
-
-            </div>
-            <div className="form-group col-sm-4">
-              <label><strong>Measurement</strong></label>
-              <select className="form-control">
-                <option>mg</option>
-              </select>
-              <select className="form-control">
-                <option>mg</option>
-              </select>
+          <form onSubmit={e => this.submitSupplementEvent(e)}>
+            <div className="row">
+              <div className="form-group col-sm-4">
+                <label><strong>Supplement Name</strong></label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Black Tea"
+                />
+              </div>
             </div>
 
-          </div>
+            <div className="row">
+              <div className="form-group col-sm-4">
+                <label><strong>Vendor</strong></label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Lipton"
+                />
 
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="form-group col-sm-4">
+                <label><strong>Supplement Composition</strong></label>
+                <select className="form-control">
+                  <option>Caffeine</option>
+                </select>
+                <select className="form-control">
+                  <option>Theanine</option>
+                </select>
+              </div>
+              <div className="form-group col-sm-4">
+                <label><strong>Quantity</strong></label>
+                <select className="form-control">
+                  <option>75</option>
+                </select>
+                <select className="form-control">
+                  <option>150</option>
+                </select>
+
+              </div>
+              <div className="form-group col-sm-4">
+                <label><strong>Measurement</strong></label>
+                <select className="form-control">
+                  <option>mg</option>
+                </select>
+                <select className="form-control">
+                  <option>mg</option>
+                </select>
+              </div>
+
+            </div>
+            <div className="float-right">
+              <button
+                type="submit"
+                id="supplement-dashboard-submit"
+                className="btn btn-sm btn-success"
+                onClick={e => this.submitSupplementEvent(e)}
+              >
+                <i className="fa fa-dot-circle-o" /> Add Supplement
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     );
