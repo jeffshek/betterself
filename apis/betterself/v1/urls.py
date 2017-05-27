1ac717cfb34f1f619898daf540b0e9216e8fa744
jeffshek <jeffshek@gmail.com>
2017-05-26 20:04:14 -0400

    Switch to trailing slashes on urls

--- apis/betterself/v1/urls.py
+++ apis/betterself/v1/urls.py
@@ -9,15 +9,15 @@ from vendors.models import Vendor
 
 urlpatterns = [
     # page looks like 127.0.0.1:8001/{Supplement.RESOURCE_NAME}/
-    url(r'^{0}'.format(Vendor.RESOURCE_NAME), VendorView.as_view(), name=Vendor.RESOURCE_NAME),
-    url(r'^{0}'.format(Supplement.RESOURCE_NAME), SupplementView.as_view(), name=Supplement.RESOURCE_NAME),
-    url(r'^{0}'.format(Ingredient.RESOURCE_NAME), IngredientView.as_view(), name=Ingredient.RESOURCE_NAME),
-    url(r'^{0}'.format(Measurement.RESOURCE_NAME), MeasurementView.as_view(), name=Measurement.RESOURCE_NAME),
-    url(r'^{0}'.format(IngredientComposition.RESOURCE_NAME), IngredientCompositionView.as_view(),
+    url(r'^{0}/$'.format(Vendor.RESOURCE_NAME), VendorView.as_view(), name=Vendor.RESOURCE_NAME),
+    url(r'^{0}/$'.format(Supplement.RESOURCE_NAME), SupplementView.as_view(), name=Supplement.RESOURCE_NAME),
+    url(r'^{0}/$'.format(Ingredient.RESOURCE_NAME), IngredientView.as_view(), name=Ingredient.RESOURCE_NAME),
+    url(r'^{0}/$'.format(Measurement.RESOURCE_NAME), MeasurementView.as_view(), name=Measurement.RESOURCE_NAME),
+    url(r'^{0}/$'.format(IngredientComposition.RESOURCE_NAME), IngredientCompositionView.as_view(),
         name=IngredientComposition.RESOURCE_NAME),
-    url(r'^{0}'.format(SupplementEvent.RESOURCE_NAME), SupplementEventView.as_view(),
+    url(r'^{0}/$'.format(SupplementEvent.RESOURCE_NAME), SupplementEventView.as_view(),
         name=SupplementEvent.RESOURCE_NAME),
-    url(r'^{0}'.format(DailyProductivityLog.RESOURCE_NAME), ProductivityLogView.as_view(),
+    url(r'^{0}/$'.format(DailyProductivityLog.RESOURCE_NAME), ProductivityLogView.as_view(),
         name=DailyProductivityLog.RESOURCE_NAME),
 ]
 
