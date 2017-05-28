ae8164a9e62313bbda1badfb85a87a2abbe3b75a
jeffshek <jeffshek@gmail.com>
2017-05-27 21:08:56 -0400

    Refactor so cleaner

--- assets/js/supplement_event_log/add_supplement_event.js
+++ assets/js/supplement_event_log/add_supplement_event.js
@@ -78,97 +78,108 @@ export class AddSupplementEvent extends Component {
     this.setState({ formSupplementDateTime: moment });
   }
 
-  render() {
+  renderCreateSupplementButton() {
+    return (
+      <div className="card-header">
+        <strong id="add-supplement-entry-text">Add Supplement Entry</strong>
+        <Link to={DASHBOARD_SUPPLEMENTS_URL}>
+          <div className="float-right">
+            <button
+              type="submit"
+              id="create-new-supplement-button"
+              className="btn btn-sm btn-success"
+            >
+              <div id="white-text">
+                <i className="fa fa-dot-circle-o" /> Create Supplement
+              </div>
+            </button>
+          </div>
+        </Link>
+      </div>
+    );
+  }
+
+  renderSubmitSupplementForm() {
     const supplementsKeys = Object.keys(this.state.supplements);
 
     return (
-      <div className="card">
-        <div className="card-header">
-          <strong id="add-supplement-entry-text">Add Supplement Entry</strong>
-          <Link to={DASHBOARD_SUPPLEMENTS_URL}>
-            <div className="float-right">
-              <button
-                type="submit"
-                id="create-new-supplement-button"
-                className="btn btn-sm btn-success"
-              >
-                <div id="white-text">
-                  <i className="fa fa-dot-circle-o" /> Create Supplement
-                </div>
-              </button>
-            </div>
-          </Link>
-        </div>
+      <div className="card-block">
+        <form onSubmit={e => this.submitSupplementEvent(e)}>
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="form-group">
+                <label className="add-event-label">Supplement</label>
+                <select
+                  className="form-control"
+                  ref={input => this.supplementNameKey = input}
+                >
+                  {/*List out all the possible supplements, use the index as the key*/}
+                  {supplementsKeys.map(key => (
+                    <option value={key} key={key}>
+                      {this.state.supplements[key].name}
+                    </option>
+                  ))}
+                </select>
 
-        <div className="card-block">
-          <form onSubmit={e => this.submitSupplementEvent(e)}>
-            <div className="row">
-              <div className="col-sm-12">
-                <div className="form-group">
-                  <label className="add-event-label">Supplement</label>
-                  <select
-                    className="form-control"
-                    ref={input => this.supplementNameKey = input}
-                  >
-                    {/*List out all the possible supplements, use the index as the key*/}
-                    {supplementsKeys.map(key => (
-                      <option value={key} key={key}>
-                        {this.state.supplements[key].name}
-                      </option>
-                    ))}
-                  </select>
-
-                </div>
               </div>
             </div>
-            <div className="row">
-              <div className="form-group col-sm-4">
+          </div>
+          <div className="row">
+            <div className="form-group col-sm-4">
+              <label className="add-event-label">
+                Quantity (Serving Size)
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                defaultValue="1"
+                ref={input => this.servingSize = input}
+              />
+            </div>
+            <div className="form-group col-sm-4">
+              <label className="add-event-label">
+                Date / Time of Ingestion
+              </label>
+              {/*Use the current datetime as a default */}
+              <Datetime
+                onChange={this.handleDateInputChange}
+                value={this.state.formSupplementDateTime}
+              />
+            </div>
+            <div className="col-sm-4">
+              <div className="form-group">
                 <label className="add-event-label">
-                  Quantity (Serving Size)
+                  Duration (Minutes)
                 </label>
                 <input
                   type="text"
                   className="form-control"
-                  defaultValue="1"
-                  ref={input => this.servingSize = input}
+                  defaultValue="0"
+                  ref={input => this.durationMinutes = input}
                 />
               </div>
-              <div className="form-group col-sm-4">
-                <label className="add-event-label">
-                  Date / Time of Ingestion
-                </label>
-                {/*Use the current datetime as a default */}
-                <Datetime
-                  onChange={this.handleDateInputChange}
-                  value={this.state.formSupplementDateTime}
-                />
-              </div>
-              <div className="col-sm-4">
-                <div className="form-group">
-                  <label className="add-event-label">
-                    Duration (Minutes)
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    defaultValue="0"
-                    ref={input => this.durationMinutes = input}
-                  />
-                </div>
-              </div>
             </div>
-            <div className="float-right">
-              <button
-                type="submit"
-                id="event-dashboard-submit"
-                className="btn btn-sm btn-success"
-                onClick={e => this.submitSupplementEvent(e)}
-              >
-                <i className="fa fa-dot-circle-o" /> Add Supplement Log
-              </button>
-            </div>
-          </form>
-        </div>
+          </div>
+          <div className="float-right">
+            <button
+              type="submit"
+              id="event-dashboard-submit"
+              className="btn btn-sm btn-success"
+              onClick={e => this.submitSupplementEvent(e)}
+            >
+              <i className="fa fa-dot-circle-o" /> Add Supplement Log
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="card">
+        {this.renderCreateSupplementButton()}
+        {this.renderSubmitSupplementForm()}
       </div>
     );
   }
