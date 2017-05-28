3d5952887a3b3f9e09050f91a3b3fe12beab7735
jeffshek <jeffshek@gmail.com>
2017-05-28 19:21:50 -0400

    Unbreak some auto renaming

--- assets/js/productivity_log/add_productivity_event.js
+++ assets/js/productivity_log/add_productivity_event.js
@@ -8,7 +8,7 @@ export class AddProductivityEvent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputDate: moment(),
+      inputDateTime: moment(),
       veryProductiveMinutes: 0,
       productiveMinutes: 0,
       neutralMinutes: 0,
--- assets/js/user_activities_events/add_user_event.js
+++ assets/js/user_activities_events/add_user_event.js
@@ -6,6 +6,7 @@ import {
 } from "../constants/util_constants";
 import moment from "moment";
 import { Link } from "react-router-dom";
+import { DASHBOARD_SUPPLEMENTS_URL } from "../urls/constants";
 
 export class AddUserActivityEvent extends Component {
   constructor(props) {
@@ -19,6 +20,7 @@ export class AddUserActivityEvent extends Component {
     this.handleDatetimeChange = this.handleDatetimeChange.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.renderInputRow = this.renderInputRow.bind(this);
+    this.renderSubmitEventForm = this.renderSubmitEventForm.bind(this);
   }
 
   componentDidMount() {
@@ -56,17 +58,24 @@ export class AddUserActivityEvent extends Component {
     const activitiesKeys = Object.keys(this.state.userActivities);
 
     return (
-      <select
-        className="form-control"
-        ref={input => this.activityNameKey = input}
-      >
-        {/*List out all the possible supplements, use the index as the key*/}
-        {activitiesKeys.map(key => (
-          <option value={key} key={key}>
-            {this.state.userActivities[key].name}
-          </option>
-        ))}
-      </select>
+      <div className="col-sm-4">
+        <div className="form-group">
+          <label className="add-event-label">
+            Activity
+          </label>
+          <select
+            className="form-control"
+            ref={input => this.activityNameKey = input}
+          >
+            {/*List out all the possible supplements, use the index as the key*/}
+            {activitiesKeys.map(key => (
+              <option value={key} key={key}>
+                {this.state.userActivities[key].name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
     );
   }
 
@@ -93,12 +102,10 @@ export class AddUserActivityEvent extends Component {
     e.preventDefault();
 
     const postParams = {
-      very_productive_time_minutes: this.state.veryProductiveMinutes,
-      productive_time_minutes: this.state.productiveMinutes,
-      neutral_time_minutes: this.state.neutralMinutes,
-      distracting_time_minutes: this.state.distractingMinutes,
-      very_distracting_time_minutes: this.state.veryDistractingMinutes,
-      time: this.state.inputDateTime.toISOString()
+      user_activity_uuid: this.state.user_activity_uuid,
+      duration_minutes: this.state.durationMinutes,
+      time: this.state.inputDateTime.toISOString(),
+      source: "web"
     };
 
     fetch("api/v1/user_activity_events/", {
@@ -118,30 +125,43 @@ export class AddUserActivityEvent extends Component {
       });
   }
 
-  render() {
+  renderCreateActivityButton() {
+    return (
+      <div className="card-header">
+        <strong id="add-supplement-entry-text">Add Activity Event</strong>
+        <Link to={DASHBOARD_SUPPLEMENTS_URL}>
+          <div className="float-right">
+            <button
+              type="submit"
+              id="create-new-supplement-button"
+              className="btn btn-sm btn-success"
+            >
+              <div id="white-text">
+                <i className="fa fa-dot-circle-o" /> Create Activity
+              </div>
+            </button>
+          </div>
+        </Link>
+      </div>
+    );
+  }
+
+  renderSubmitEventForm() {
     return (
       <div className="card">
-        <div className="card-header">
-          <strong id="add-supplement-entry-text">
-            Log Event
-          </strong>
-        </div>
-
         <div className="card-block">
           <form onSubmit={e => this.submitEventDetails(e)}>
             <div className="form-group col-sm-4">
               <label className="add-event-label">
-                Date
+                Time
               </label>
               <Datetime
                 onChange={this.handleDatetimeChange}
                 value={this.state.inputDateTime}
               />
             </div>
-
             {this.renderActivitySelect()}
             {this.renderInputRow("Duration (Minutes)", "durationMinutes")}
-
             <div className="float-right">
               <button
                 type="submit"
@@ -154,6 +174,15 @@ export class AddUserActivityEvent extends Component {
             </div>
           </form>
         </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderCreateActivityButton()}
+        {this.renderSubmitEventForm()}
       </div>
     );
   }
