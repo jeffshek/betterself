c7ab2e20a083989f3dabf41ed562cded575b3ff8
Jeffrey Shek <jeffshek@gmail.com>
2017-06-03 22:03:53 -0400

    Edits to Home Page (#64)
    * Correct for features coming soon but not quite there yet
    * Simpler and cleaner landing page
    * Lot of home page modifications, more shame CSS moments

--- assets/js/home/components/HomePageAnalyticsDescriptionSection.js
+++ assets/js/home/components/HomePageAnalyticsDescriptionSection.js
@@ -1,47 +1,7 @@
-import React, { PropTypes, Component } from "react";
+import React, { Component, PropTypes } from "react";
 import CSSModules from "react-css-modules";
 import styles from "../css/HomePageAnalyticsDescriptionSection.css";
-import {
-  DASHBOARD_EXAMPLE_PATH,
-  DASHBOARD_HEART_RATE,
-  DASHBOARD_SUPPLEMENTS_HISTORY
-} from "../../constants/image_paths";
-import Slider from "react-slick";
-
-class SimpleSlider extends React.Component {
-  render() {
-    const settings = {
-      infinite: true,
-      accessibility: true,
-      adaptiveHeight: false,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-    return (
-      <Slider {...settings}>
-        <div>
-          <img
-            className="img-responsive dashboard-example"
-            src={DASHBOARD_EXAMPLE_PATH}
-          />
-        </div>
-        <div>
-          <img
-            className="img-responsive dashboard-example"
-            src={DASHBOARD_HEART_RATE}
-          />
-        </div>
-        <div>
-          <img
-            className="img-responsive dashboard-example"
-            src={DASHBOARD_SUPPLEMENTS_HISTORY}
-          />
-        </div>
-      </Slider>
-    );
-  }
-}
+import { DASHBOARD_EXAMPLE_PATH } from "../../constants/image_paths";
 
 class HomePageAnalyticsDescriptionSection extends React.Component {
   render() {
@@ -49,20 +9,22 @@ class HomePageAnalyticsDescriptionSection extends React.Component {
       <section styleName="description-block">
         <div className="container">
           <div className="row">
-            <div className="col-lg-5 col-md-6 col-sm-12">
+            <div className="col-lg-4 col-md-3 col-sm-12">
               <h1>Personalized Analytics</h1>
               <p styleName="lead-white">
-                Ever wonder how your body is being impacted by your habits and diet? How sleep impacts decision making?
+                Ever wonder how your body is being impacted by supplements, diet or habits? How sleep impacts decision making?
                 {" "}
               </p>
               <p styleName="lead-white">
-                Track your decisions, activities, and supplements to see what's really impacting you. What do your most creative days and weeks have in common? You've come to the right place.
-              </p>
-              <p styleName="lead-white">
-                We collect no personal info. Open-sourced to give you the peace of mind that your data isn't being manipulated. All of your data can be safely exported in one easy click. You can also safely delete all your data in the control panel.
+                Track decisions, activities, and supplements to see what's really impacting you. What do your most creative days and weeks have in common? You've come to the right place.
               </p>
             </div>
-            <SimpleSlider />
+            <div className="col-lg-8">
+              <img
+                className="img-responsive dashboard-example"
+                src={DASHBOARD_EXAMPLE_PATH}
+              />
+            </div>
           </div>
         </div>
       </section>
--- assets/js/home/components/HomePageDescription.js
+++ assets/js/home/components/HomePageDescription.js
@@ -19,7 +19,7 @@ class HomePageDescriptionSection extends React.Component {
             <div className="col-md-6 col-md-offset-6 col-sm-8 col-sm-offset-4 content clearfix">
               <h1>Your Body's Dashboard</h1>
               <p styleName="content-1-4-lead">
-                Track supplements, sleep, productivity, heart rate, exercise, and happiness across fifteen integrations.
+                Track supplements, sleep, productivity, heart rate, exercise, and happiness across multiple integrations.
                 <br />
               </p>
               {/*Force redirect to take a different CSS, aka why we don't use Link*/}
@@ -28,6 +28,13 @@ class HomePageDescriptionSection extends React.Component {
                 className="btn btn-outline btn-outline-lg outline-dark"
               >
                 Try the demo
+              </a>
+              &nbsp; or &nbsp;
+              <a
+                href={DASHBOARD_INDEX_URL}
+                className="btn btn-outline btn-outline-lg outline-dark"
+              >
+                Sign up (free)
               </a>
               <div className="row pad45">
                 <div className="col-sm-6">
