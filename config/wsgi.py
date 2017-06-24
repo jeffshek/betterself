013680345cd0ccecc84c1952c8c6f9ce83bd9475
jeffshek <jeffshek@gmail.com>
2017-06-24 15:24:06 +0200

    Another attempt at missing files for WhiteNoise

--- config/wsgi.py
+++ config/wsgi.py
@@ -17,9 +17,6 @@ import os
 from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
-if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
-    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
@@ -30,7 +27,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
 if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
+    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+
     application = Sentry(application)
     # see if this helps performance behind CloudFront
-    # application = DjangoWhiteNoise(application)
+    application = DjangoWhiteNoise(application)
