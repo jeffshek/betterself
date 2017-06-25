ec7dd3ed3bf1fdbbc041a95061da0dbeb399708d
jeffshek <jeffshek@gmail.com>
2017-06-26 01:00:33 +0200

    Update and switch storage to whitenoise recc

--- config/settings/production.py
+++ config/settings/production.py
@@ -135,8 +135,8 @@ AWS_HEADERS = {
 
 # Static Assets
 # ------------------------
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # EMAIL
