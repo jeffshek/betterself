42e01cb671b9ced1a726a7ae5ba0cde9353a9c53
jeffshek <jeffshek@gmail.com>
2017-06-30 22:19:21 -0400

    Add basic reverse URL for sleep views

--- apis/betterself/v1/events/tests/test_sleep_views.py
+++ apis/betterself/v1/events/tests/test_sleep_views.py
@@ -1,7 +1,41 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+from apis.betterself.v1.signup.fixtures.builders import DemoHistoricalDataBuilder
+
 # python manage.py test apis.betterself.v1.events.tests.test_sleep_views
+User = get_user_model()
 
 
 class ActivityTests(TestCase):
-    def test_activity_view(self):
-        pass
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(username='demo')
+
+        # kinda wish you had done this via a factory like everything else
+        builder = DemoHistoricalDataBuilder(cls.user)
+        builder.create_historical_fixtures()
+
+        super().setUpTestData()
+
+    def setUp(self):
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_sleep_aggregates_view(self):
+        url = reverse('sleep-aggregates')
+        print (url)
+
+    def test_sleep_averages_view(self):
+        url = reverse('sleep-averages')
+        print (url)
+
+    def test_sleep_activities_correlations_view(self):
+        url = reverse('sleep-activities-correlations')
+        print (url)
+
+    def test_sleep_supplements_view(self):
+        url = reverse('sleep-supplements-correlations')
+        print (url)
--- apis/betterself/v1/urls.py
+++ apis/betterself/v1/urls.py
@@ -30,10 +30,10 @@ urlpatterns = [
     url(r'^{0}/'.format(SleepActivity.RESOURCE_NAME),
         include([
             url(r'^$', SleepActivityView.as_view(), name=SleepActivity.RESOURCE_NAME),
-            url(r'^aggregates$', SleepAggregatesView.as_view(), name=SleepActivity.RESOURCE_NAME),
-            url(r'^averages$', SleepAveragesView.as_view(), name=SleepActivity.RESOURCE_NAME),
-            url(r'^activities/correlations$', SleepActivitiesCorrelationView.as_view(), name=SleepActivity.RESOURCE_NAME),  # noqa
-            url(r'^supplements/correlations$', SleepSupplementsCorrelationView.as_view(), name=SleepActivity.RESOURCE_NAME),  # noqa
+            url(r'^aggregates$', SleepAggregatesView.as_view(), name='sleep-aggregates'),
+            url(r'^averages$', SleepAveragesView.as_view(), name='sleep-averages'),
+            url(r'^activities/correlations$', SleepActivitiesCorrelationView.as_view(), name='sleep-activities-correlations'),  # noqa
+            url(r'^supplements/correlations$', SleepSupplementsCorrelationView.as_view(), name='sleep-supplements-correlations'),  # noqa
         ])),
     # The pages below are used by the front-end to create API requests that do business logic
     url(r'user-signup/$', CreateUserView.as_view(), name='api-create-user'),
