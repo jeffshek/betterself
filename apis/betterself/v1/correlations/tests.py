d3f12115b65e323861096d731dd1081e3539014e
jeffshek <jeffshek@gmail.com>
2017-07-08 19:40:19 -0400

    Make correlations tests pretty

--- apis/betterself/v1/correlations/tests.py
+++ apis/betterself/v1/correlations/tests.py
@@ -13,7 +13,22 @@ User = get_user_model()
 
 # python manage.py test apis.betterself.v1.correlations.tests
 
-class BaseCorrelationTests(TestCase):
+class BaseCorrelationsMixin(object):
+    def test_view_with_user_and_no_data(self):
+        user = User.objects.create(username='something-new')
+        client = APIClient()
+        client.force_authenticate(user)
+
+        response = client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+
+class BaseCorrelationsTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.url = reverse(cls.url_namespace)
+        super().setUpClass()
+
     @classmethod
     def setUpTestData(cls):
         cls.user = User.objects.create(username='demo')
@@ -26,21 +41,21 @@ class BaseCorrelationTests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.client.force_authenticate(self.user)
+        super().setUp()
 
 
-class ProductivitySupplementCorrelationTests(BaseCorrelationTests):
+class ProductivitySupplementCorrelationsTests(BaseCorrelationsTests, BaseCorrelationsMixin):
+    url_namespace = 'productivity-supplements-correlations'
+
     def test_productivity_supplements_correlation_view(self):
-        url = reverse('productivity-supplements-correlations')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
-        # the correlation of the productivity driver (which will be the first result of the dataset)
-        # will be 1
+        # the correlation of the productivity driver (which will be the first result of the dataset) will be 1
         self.assertEqual(response.data[0][1], 1)
 
     def test_productivity_supplements_response_includes_correct_supplements(self):
-        url = reverse('productivity-supplements-correlations')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
 
         supplements_in_response = [item[0] for item in response.data]
         # don't include the productivity driver since that's not a supplement
@@ -55,29 +70,20 @@ class ProductivitySupplementCorrelationTests(BaseCorrelationTests):
         self.assertCountEqual(supplements_in_response, user_supplements)
 
     def test_productivity_supplements_correlation_with_empty_user(self):
-        url = reverse('productivity-supplements-correlations')
-
         user = User.objects.create(username='something-new')
         client = APIClient()
         client.force_authenticate(user)
 
-        response = client.get(url)
+        response = client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertIsNone(response.data)
 
 
-class SleepCorrelationTests(BaseCorrelationTests):
-    def test_sleep_activities_view(self):
-        url = reverse('sleep-user-activities-correlations')
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, 200)
-        # the correlation of sleep to itself will be one
-        self.assertEqual(response.data[0][1], 1)
+class SleepSupplementsCorrelationsTests(BaseCorrelationsTests, BaseCorrelationsMixin):
+    url_namespace = 'sleep-supplements-correlations'
 
     def test_sleep_supplements_view(self):
-        url = reverse('sleep-supplements-correlations')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
 
         self.assertTrue(SLEEP_MINUTES_COLUMN in response.data[0])
         # Return back in a tuple format to preserve order when transmitting as JSON
@@ -85,38 +91,40 @@ class SleepCorrelationTests(BaseCorrelationTests):
         self.assertEqual(response.data[0][1], 1)
         self.assertEqual(response.status_code, 200)
 
-    def test_sleep_activities_with_empty_user(self):
-        url = reverse('sleep-user-activities-correlations')
-
-        user = User.objects.create(username='something-new')
-        client = APIClient()
-        client.force_authenticate(user)
-
-        response = client.get(url)
-        self.assertEqual(response.status_code, 200)
-
     def test_sleep_supplements_with_empty_user(self):
-        url = reverse('sleep-supplements-correlations')
-
         user = User.objects.create(username='something-new')
         client = APIClient()
         client.force_authenticate(user)
 
-        response = client.get(url)
+        response = client.get(self.url)
         self.assertEqual(response.status_code, 200)
 
 
-class TestProductivityUserActivitiesCorrelations(BaseCorrelationTests):
-    pass
-    # @classmethod
-    # def setUpTestData(cls):
-    #     cls.user = User.objects.create(username='demo')
-    #
-    #     builder = DemoHistoricalDataBuilder(cls.user)
-    #     builder.create_historical_fixtures()
-    #
-    #     super().setUpTestData()
-    #
-    # def setUp(self):
-    #     self.client = APIClient()
-    #     self.client.force_authenticate(self.user)
+class SleepUserActivitiesCorrelationsTests(BaseCorrelationsTests, BaseCorrelationsMixin):
+    url_namespace = 'sleep-user-activities-correlations'
+
+    def test_sleep_activities_view(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        # the correlation of sleep to itself will be one
+        self.assertEqual(response.data[0][1], 1)
+
+    def test_sleep_activities_with_empty_user(self):
+        user = User.objects.create(username='something-new')
+        client = APIClient()
+        client.force_authenticate(user)
+
+        response = client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+
+class TestProductivityUserActivitiesCorrelations(BaseCorrelationsTests, BaseCorrelationsMixin):
+    url_namespace = 'productivity-user-activities-correlations'
+
+    def test_correlations_view(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        # the correlation of a variable to itself will be one
+        self.assertEqual(response.data[0][1], 1)
