1bf586b0bc603c9d6f429b55ed69be49b15d90a4
Jeffrey Shek <jeffshek@gmail.com>
2017-07-08 02:05:06 -0400

    Productivity Analytics Frontend Part #2 (#87)
    * Working dropdown for Productivity History Chart
    * Dump columns into one constants file
    * Continue focus toward using constants
    * Add working supplements and productivity correlation

--- assets/js/analytics/productivity.js
+++ assets/js/analytics/productivity.js
@@ -3,24 +3,207 @@ import { Bar, Doughnut, Line, Pie, Polar, Radar } from "react-chartjs-2";
 import { Nav, NavItem, NavLink } from "reactstrap";
 import { JSON_AUTHORIZATION_HEADERS } from "../constants/util_constants";
 import moment from "moment";
-import { DefaultLineChartDataset } from "../constants/charts";
+import {
+  CHART_HOVER_BORDER_COLOR,
+  CHART_HOVER_COLOR,
+  CHARTS_BACKGROUND_COLOR,
+  DataAnalyticsRow,
+  DefaultLineChartDataset
+} from "../constants/charts";
+import {
+  DISTRACTING_MINUTES_LABEL,
+  DISTRACTING_MINUTES_VARIABLE,
+  NEGATIVELY_CORRELATED_LABEL,
+  NEUTRAL_MINUTES_LABEL,
+  NEUTRAL_MINUTES_VARIABLE,
+  POSITIVELY_CORRELATED_LABEL,
+  PRODUCTIVE_MINUTES_LABEL,
+  PRODUCTIVE_MINUTES_VARIABLE,
+  VERY_DISTRACTING_MINUTES_LABEL,
+  VERY_DISTRACTING_MINUTES_VARIABLE,
+  VERY_PRODUCTIVE_MINUTES_LABEL,
+  VERY_PRODUCTIVE_MINUTES_VARIABLE
+} from "../constants";
+
+const ProductivityColumnMappingToKey = {
+  "Very Productive Minutes": VERY_PRODUCTIVE_MINUTES_VARIABLE,
+  "Productive Minutes": PRODUCTIVE_MINUTES_VARIABLE,
+  "Neutral Minutes": NEUTRAL_MINUTES_VARIABLE,
+  "Distracting Minutes": DISTRACTING_MINUTES_VARIABLE,
+  "Very Distracting Minutes": VERY_DISTRACTING_MINUTES_VARIABLE
+};
 
 const ProductivityHistoryChart = {
   labels: [],
   datasets: [Object.assign({}, DefaultLineChartDataset)]
 };
 
+const SupplementsCorrelationsChart = {
+  labels: [],
+  datasets: [
+    {
+      label: "Very Productive Minutes Correlation",
+      backgroundColor: CHARTS_BACKGROUND_COLOR,
+      borderColor: CHARTS_BACKGROUND_COLOR,
+      borderWidth: 1,
+      hoverBackgroundColor: CHART_HOVER_COLOR,
+      hoverBorderColor: CHART_HOVER_BORDER_COLOR,
+      data: []
+    }
+  ]
+};
+
+const ActivitiesCorrelationsChart = {
+  labels: [],
+  datasets: [
+    {
+      label: "Productivity Correlation",
+      backgroundColor: CHARTS_BACKGROUND_COLOR,
+      borderColor: CHARTS_BACKGROUND_COLOR,
+      borderWidth: 1,
+      hoverBackgroundColor: CHART_HOVER_COLOR,
+      hoverBorderColor: CHART_HOVER_BORDER_COLOR,
+      data: []
+    }
+  ]
+};
+
 export class ProductivityAnalyticsView extends Component {
   constructor() {
     super();
     this.state = {
-      productivityHistory: ProductivityHistoryChart
+      productivityHistoryChart: ProductivityHistoryChart,
+      //
+      selectedProductivityHistoryChartData: [],
+      selectedProductivityHistoryType: VERY_PRODUCTIVE_MINUTES_LABEL,
+      //
+      supplementsCorrelationsChart: SupplementsCorrelationsChart,
+      selectedSupplementsCorrelations: [],
+      selectedSupplementsCorrelationsTab: POSITIVELY_CORRELATED_LABEL,
+      positiveSupplementsCorrelations: [],
+      negativeSupplementsCorrelations: [],
+      //
+      selectedUserActivityCorrelationsChart: ActivitiesCorrelationsChart,
+      selectedUserActivitiesCorrelations: [],
+      selectedUserActivitiesCorrelationsTab: POSITIVELY_CORRELATED_LABEL,
+      positiveUserActivitiesCorrelations: [],
+      negativeUserActivitiesCorrelations: []
     };
-    this.state.productivityHistory.datasets[0].label = "Time (Minutes)";
+    this.state.productivityHistoryChart.datasets[
+      0
+    ].label = this.state.selectedProductivityHistoryType;
+    this.handleSelectedProductivityHistory = this.handleSelectedProductivityHistory.bind(
+      this
+    );
+    this.selectSupplementsCorrelationsTab = this.selectSupplementsCorrelationsTab.bind(
+      this
+    );
   }
 
   componentDidMount() {
     this.getHistory();
+    this.getSupplementsCorrelations();
+  }
+
+  handleSelectedProductivityHistory(event) {
+    const selectedProductivityHistoryType = event.target.value;
+    const column_key =
+      ProductivityColumnMappingToKey[selectedProductivityHistoryType];
+
+    const arrayValues = this.state.selectedProductivityHistoryChartData.map(
+      key => key[column_key]
+    );
+
+    this.state.selectedProductivityHistoryType = selectedProductivityHistoryType;
+    this.state.productivityHistoryChart.datasets[0].data = arrayValues;
+    this.state.productivityHistoryChart.datasets[
+      0
+    ].label = this.state.selectedProductivityHistoryType;
+
+    this.setState({
+      productivityHistoryChart: this.state.productivityHistoryChart
+    });
+  }
+
+  //
+  // API Calls
+  //
+  getSupplementsCorrelations() {
+    fetch(`api/v1/productivity_log/supplements/correlations`, {
+      method: "GET",
+      headers: JSON_AUTHORIZATION_HEADERS
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        const labels = responseData.map(data => {
+          return data[0];
+        });
+        const dataValues = responseData.map(data => {
+          return data[1];
+        });
+
+        this.state.supplementsCorrelationsChart.labels = labels;
+        this.state.supplementsCorrelationsChart.datasets[0].data = dataValues;
+
+        const positivelyCorrelatedSupplements = responseData.filter(data => {
+          return data[1] > 0;
+        });
+        const negativelyCorrelatedSupplements = responseData.filter(data => {
+          return data[1] < 0;
+        });
+
+        // Finally update state after we've done so much magic
+        this.setState({
+          supplementsCorrelationsChart: this.state.supplementsCorrelationsChart,
+          selectedSupplementsCorrelations: positivelyCorrelatedSupplements,
+          positiveSupplementsCorrelations: positivelyCorrelatedSupplements,
+          negativeSupplementsCorrelations: negativelyCorrelatedSupplements
+        });
+      });
+  }
+
+  selectSupplementsCorrelationsTab(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const name = target.name;
+
+    if (name === POSITIVELY_CORRELATED_LABEL) {
+      this.setState({
+        selectedSupplementsCorrelations: this.state
+          .positiveSupplementsCorrelations
+      });
+    } else if (name === NEGATIVELY_CORRELATED_LABEL) {
+      this.setState({
+        selectedSupplementsCorrelations: this.state
+          .negativeSupplementsCorrelations
+      });
+    }
+
+    this.setState({
+      selectedSupplementsCorrelationsTab: name
+    });
+  }
+
+  renderSupplementsCorrelationSelectionTab(tabName) {
+    if (this.state.selectedSupplementsCorrelationsTab === tabName) {
+      return (
+        <NavItem className="selected-modal">
+          <NavLink>
+            {tabName}
+          </NavLink>
+        </NavItem>
+      );
+    }
+    return (
+      <NavItem className="default-background">
+        <NavLink onClick={this.selectSupplementsCorrelationsTab} name={tabName}>
+          {tabName}
+        </NavLink>
+      </NavItem>
+    );
   }
 
   getHistory() {
@@ -33,38 +216,96 @@ export class ProductivityAnalyticsView extends Component {
       })
       .then(responseData => {
         const reverseResponseData = responseData.results.reverse();
-        console.log(reverseResponseData);
+
         const labelDates = reverseResponseData.map(key => key.date);
         const arrayValues = reverseResponseData.map(
           key => key.very_productive_time_minutes
         );
 
-        this.state.productivityHistory.labels = labelDates;
-        this.state.productivityHistory.datasets[0].data = arrayValues;
+        this.state.productivityHistoryChart.labels = labelDates;
+        this.state.productivityHistoryChart.datasets[0].data = arrayValues;
 
-        this.setState({ productivityHistory: this.state.productivityHistory });
+        this.setState({
+          productivityHistoryChart: this.state.productivityHistoryChart,
+          selectedProductivityHistoryChartData: reverseResponseData
+        });
       });
   }
+
+  renderSupplementsCorrelations() {
+    return (
+      <div className="card-columns cols-2">
+        <div className="card">
+          <div className="card-header analytics-text-box-label">
+            Supplements and Productivity Correlation
+          </div>
+          <div className="card-block">
+            <div className="chart-wrapper">
+              <Bar
+                data={SupplementsCorrelationsChart}
+                options={{
+                  maintainAspectRatio: true
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="float">
+          <div className="card">
+            <Nav tabs>
+              {this.renderSupplementsCorrelationSelectionTab(
+                POSITIVELY_CORRELATED_LABEL
+              )}
+              {this.renderSupplementsCorrelationSelectionTab(
+                NEGATIVELY_CORRELATED_LABEL
+              )}
+            </Nav>
+            <div className="card-block">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Supplement</th>
+                    <th>Correlation</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.selectedSupplementsCorrelations.map(key => (
+                    <DataAnalyticsRow key={key} object={key} />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   renderHistoryChart() {
     return (
       <div className="card">
         <div className="card-header analytics-text-box-label">
           <span className="font-2xl">Productivity History</span>
           <span className="float-right">
-            <input
-              type="text"
-              id="text-input"
-              name="text-input"
-              className="form-control center-source"
-              placeholder="Very Productive"
-            />
+            Chart Selection
+            <select
+              className="form-control chart-selector"
+              onChange={this.handleSelectedProductivityHistory}
+              value={this.state.selectedProductivityHistoryType}
+              size="1"
+            >
+              <option>{VERY_PRODUCTIVE_MINUTES_LABEL}</option>
+              <option>{PRODUCTIVE_MINUTES_LABEL}</option>
+              <option>{NEUTRAL_MINUTES_LABEL}</option>
+              <option>{DISTRACTING_MINUTES_LABEL}</option>
+              <option>{VERY_DISTRACTING_MINUTES_LABEL}</option>
+            </select>
           </span>
         </div>
-
         <div className="card-block">
           <div className="chart-wrapper">
             <Line
-              data={this.state.productivityHistory}
+              data={this.state.productivityHistoryChart}
               options={{
                 maintainAspectRatio: false
               }}
@@ -79,6 +320,7 @@ export class ProductivityAnalyticsView extends Component {
     return (
       <div className="animated fadeIn">
         {this.renderHistoryChart()}
+        {this.renderSupplementsCorrelations()}
       </div>
     );
   }
---
+++ assets/js/constants.js
@@ -0,0 +1,15 @@
+export const VERY_PRODUCTIVE_MINUTES_LABEL = "Very Productive Minutes";
+export const PRODUCTIVE_MINUTES_LABEL = "Productive Minutes";
+export const NEUTRAL_MINUTES_LABEL = "Neutral Minutes";
+export const DISTRACTING_MINUTES_LABEL = "Distracting Minutes";
+export const VERY_DISTRACTING_MINUTES_LABEL = "Very Distracting Minutes";
+
+export const VERY_PRODUCTIVE_MINUTES_VARIABLE = "very_productive_time_minutes";
+export const PRODUCTIVE_MINUTES_VARIABLE = "productive_time_minutes";
+export const NEUTRAL_MINUTES_VARIABLE = "neutral_time_minutes";
+export const DISTRACTING_MINUTES_VARIABLE = "distracting_time_minutes";
+export const VERY_DISTRACTING_MINUTES_VARIABLE =
+  "very_distracting_time_minutes";
+
+export const POSITIVELY_CORRELATED_LABEL = "Positively Correlated";
+export const NEGATIVELY_CORRELATED_LABEL = "Negatively Correlated";
