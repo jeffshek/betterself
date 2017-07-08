bbc4779f793b5d2f5f0584ddd001abbe45f9f9eb
jeffshek <jeffshek@gmail.com>
2017-07-08 01:02:20 -0400

    Working dropdown for Productivity History Chart

--- assets/js/analytics/productivity.js
+++ assets/js/analytics/productivity.js
@@ -10,13 +10,44 @@ const ProductivityHistoryChart = {
   datasets: [Object.assign({}, DefaultLineChartDataset)]
 };
 
+const ProductivityColumnMappingToKey = {
+  "Very Productive Minutes": "very_productive_time_minutes",
+  "Productive Minutes": "productive_time_minutes",
+  "Neutral Minutes": "neutral_time_minutes",
+  "Distracting Minutes": "distracting_time_minutes",
+  "Very Distracting Minutes": "very_distracting_time_minutes"
+};
+
 export class ProductivityAnalyticsView extends Component {
   constructor() {
     super();
     this.state = {
+      chartHistoryData: [],
+      selectedProductivityHistoryType: "Very Productive Minutes",
       productivityHistory: ProductivityHistoryChart
     };
-    this.state.productivityHistory.datasets[0].label = "Time (Minutes)";
+    this.state.productivityHistory.datasets[
+      0
+    ].label = this.state.selectedProductivityHistoryType;
+    this.handleSelectedProductivityHistory = this.handleSelectedProductivityHistory.bind(
+      this
+    );
+  }
+
+  handleSelectedProductivityHistory(event) {
+    const selectedProductivityHistoryType = event.target.value;
+    const column_key =
+      ProductivityColumnMappingToKey[selectedProductivityHistoryType];
+
+    const arrayValues = this.state.chartHistoryData.map(key => key[column_key]);
+
+    this.state.selectedProductivityHistoryType = selectedProductivityHistoryType;
+    this.state.productivityHistory.datasets[0].data = arrayValues;
+    this.state.productivityHistory.datasets[
+      0
+    ].label = this.state.selectedProductivityHistoryType;
+
+    this.setState({ productivityHistory: this.state.productivityHistory });
   }
 
   componentDidMount() {
@@ -33,7 +64,7 @@ export class ProductivityAnalyticsView extends Component {
       })
       .then(responseData => {
         const reverseResponseData = responseData.results.reverse();
-        console.log(reverseResponseData);
+
         const labelDates = reverseResponseData.map(key => key.date);
         const arrayValues = reverseResponseData.map(
           key => key.very_productive_time_minutes
@@ -42,7 +73,10 @@ export class ProductivityAnalyticsView extends Component {
         this.state.productivityHistory.labels = labelDates;
         this.state.productivityHistory.datasets[0].data = arrayValues;
 
-        this.setState({ productivityHistory: this.state.productivityHistory });
+        this.setState({
+          productivityHistory: this.state.productivityHistory,
+          chartHistoryData: reverseResponseData
+        });
       });
   }
   renderHistoryChart() {
@@ -51,16 +85,21 @@ export class ProductivityAnalyticsView extends Component {
         <div className="card-header analytics-text-box-label">
           <span className="font-2xl">Productivity History</span>
           <span className="float-right">
-            <input
-              type="text"
-              id="text-input"
-              name="text-input"
-              className="form-control center-source"
-              placeholder="Very Productive"
-            />
+            Chart Selection
+            <select
+              className="form-control chart-selector"
+              onChange={this.handleSelectedProductivityHistory}
+              value={this.state.selectedProductivityHistoryType}
+              size="1"
+            >
+              <option>Very Productive Minutes</option>
+              <option>Productive Minutes</option>
+              <option>Neutral Minutes</option>
+              <option>Distracting Minutes</option>
+              <option>Very Distracting Minutes</option>
+            </select>
           </span>
         </div>
-
         <div className="card-block">
           <div className="chart-wrapper">
             <Line
