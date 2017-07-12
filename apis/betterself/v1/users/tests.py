98f4aaf7be269004adc4fc7c6791aa7e7931a101
jeffshek <jeffshek@gmail.com>
2017-07-12 01:28:53 -0400

    Add more tests around edge cases and exporting data

--- apis/betterself/v1/users/tests.py
+++ apis/betterself/v1/users/tests.py
@@ -11,6 +11,7 @@ User = get_user_model()
 class UserExportAllDataTests(TestCase):
     @classmethod
     def setUpTestData(cls):
+        cls.url = reverse('api-user-export-all-data')
         cls.user = User.objects.create(username='demo')
 
         builder = DemoHistoricalDataBuilder(cls.user)
@@ -25,11 +26,23 @@ class UserExportAllDataTests(TestCase):
     def test_export_view(self):
         # TODO - switch to loading the response in an excel reader
         # and measuring the columns are equal to what one would expect
-        url = reverse('api-user-export-all-data')
-
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         response_content_disposition = response.get('Content-Disposition')
 
         self.assertEqual(response.status_code, 200)
         self.assertTrue('attachment; filename=' in response_content_disposition)
         self.assertTrue('xlsx' in response_content_disposition)
+
+    def test_export_view_with_user_and_no_data(self):
+        user = User.objects.create(username='demo-with-no-data')
+        client = APIClient()
+        client.force_authenticate(user)
+
+        response = client.get(self.url)
+        print (response.status_code)
+
+    def test_export_view_not_logged_in(self):
+        client = APIClient()
+        response = client.get(self.url)
+        # not authorized
+        self.assertEqual(response.status_code, 403)
