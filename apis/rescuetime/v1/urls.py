1db701494a0c416121b7e3c18b2269a8a85cc97c
jeffshek <jeffshek@gmail.com>
2017-07-26 21:33:45 -0400

    PR Comments

--- apis/rescuetime/v1/urls.py
+++ apis/rescuetime/v1/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from apis.rescuetime.v1.views import UpdateRescueTimeAPIView
 
 urlpatterns = [
-    # page looks like 127.0.0.1:8001/{Supplement.RESOURCE_NAME}/
-    url(r'^rescuetime/update-productivity-history$', UpdateRescueTimeAPIView.as_view(), name='rescuetime-user-update-productivity-history'),  # noqa
+    # /api/v1/rescuetime/update-productivity-history
+    url(r'^update-productivity-history$', UpdateRescueTimeAPIView.as_view(), name='rescuetime-user-update-productivity-history'),  # noqa
 ]
