53a292a66264ff52ba4e4cbb3aa1dc30595261e3
jeffshek <jeffshek@gmail.com>
2017-07-27 22:01:10 -0400

    Pull request comments

--- apis/rescuetime/v1/serializers.py
+++ apis/rescuetime/v1/serializers.py
@@ -6,3 +6,11 @@ class RescueTimeAPIRequestSerializer(serializers.Serializer):
     # add a check to make sure end_date is greater than start_date
     start_date = serializers.DateField()
     end_date = serializers.DateField()
+
+    def validate(self, data):
+        """
+        Check that the start is before the stop.
+        """
+        if data['start_date'] > data['end_date']:
+            raise serializers.ValidationError('Finish must occur after start')
+        return data
