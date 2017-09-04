a60e9971e7b7edc9677f8f8d275d05bf29ae07ed
jeffshek <jeffshek@gmail.com>
2017-09-03 20:09:24 -0400

    Remove unusued variable

--- assets/js/analytics/base.js
+++ assets/js/analytics/base.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Bar, Doughnut, Line, Pie, Polar, Radar } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 import { Nav, NavItem, NavLink } from "reactstrap";
 import { JSON_AUTHORIZATION_HEADERS } from "../constants/requests";
 import {
