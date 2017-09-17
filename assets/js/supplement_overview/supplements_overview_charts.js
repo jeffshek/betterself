0ed1a373d689b2322932953b414760a0e7509aaf
Jeffrey Shek <jeffshek@gmail.com>
2017-09-17 03:59:56 -0400

    Fix Graphs

--- assets/js/supplement_overview/supplements_overview_charts.js
+++ assets/js/supplement_overview/supplements_overview_charts.js
@@ -32,7 +32,7 @@ export class SupplementsAndProductivityChartView extends Component {
 
   // Add start dates, we only need to go 3 months back!
   updateDailyProductivityData() {
-    const url = `/api/v1/productivity_log/aggregates/`;
+    const url = `/api/v1/productivity_log/aggregates/?complete_date_range_in_daily_frequency=True`;
 
     fetch(url, {
       method: "GET",
