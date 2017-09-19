92bf908b65415ba21706a850286ad2163f644855
Jeffrey Shek <jeffshek@gmail.com>
2017-09-19 01:02:03 -0400

    Remove Features To Make Things Simpler, Add Hotjar Tracking To Diagnose Traffic (#143)

--- assets/js/constants/dates_and_times.js
+++ assets/js/constants/dates_and_times.js
@@ -2,6 +2,7 @@ export const YEAR_MONTH_DAY_FORMAT = "MMMM Do YYYY";
 export const READABLE_DATE_TIME_FORMAT = "dddd, MMMM Do YYYY, h:mm:ss a";
 export const READABLE_TIME_FORMAT = "h:mm:ss a";
 export const DATE_REQUEST_FORMAT = "YYYY-MM-DD";
+export const DATETIME_CREATED_FORMAT = "l - h:mm:ss a";
 
 // 12/5/2016 Tu (this is what it looks like)
 export const ABBREVIATED_CHART_DATE = "l dd";
--- assets/js/supplements/add_supplement.js
+++ assets/js/supplements/add_supplement.js
@@ -5,84 +5,20 @@ import {
   JSON_POST_AUTHORIZATION_HEADERS
 } from "../constants/requests";
 
-// All the code in this file is embarassing
-
 export class AddSupplementView extends Component {
   constructor() {
     super();
     this.state = { ready: false };
-    this.addSupplementFormData = this.addSupplementFormData.bind(this);
-    this.postIngredientComposition = this.postIngredientComposition.bind(this);
-
-    this.createSupplement = this.createSupplement.bind(this);
-    this.createIngredientComposition = this.createIngredientComposition.bind(
-      this
-    );
-
-    this.createIngredientComposition1 = this.createIngredientComposition1.bind(
-      this
-    );
-    this.createIngredientComposition2 = this.createIngredientComposition2.bind(
-      this
-    );
-    this.createIngredientComposition3 = this.createIngredientComposition3.bind(
-      this
-    );
   }
 
   componentDidMount() {
     this.getPossibleSupplements();
   }
 
-  createIngredient(ingredientName) {
-    const params = {
-      name: ingredientName
-    };
-    return fetch("/api/v1/ingredients/", {
-      method: "POST",
-      headers: JSON_POST_AUTHORIZATION_HEADERS,
-      body: JSON.stringify(params)
-    })
-      .then(response => {
-        return response.json();
-      })
-      .then(responseData => {
-        // Update the page after success
-        window.location.refresh();
-      });
-  }
-
-  postIngredientComposition(
-    ingredient,
-    ingredientQuantity,
-    ingredientMeasurement
-  ) {
-    const params = {
-      ingredient_uuid: ingredient.uuid,
-      measurement_uuid: ingredientMeasurement.uuid,
-      quantity: ingredientQuantity
-    };
-
-    return fetch("/api/v1/ingredient_compositions/", {
-      method: "POST",
-      headers: JSON_POST_AUTHORIZATION_HEADERS,
-      body: JSON.stringify(params)
-    })
-      .then(response => {
-        return response.json();
-      })
-      .then(responseData => {
-        return responseData;
-      });
-  }
-
-  createSupplement(supplementName, ingredientCompositions) {
+  createSupplement = supplementName => {
     let params = {
       name: supplementName
     };
-    if (ingredientCompositions) {
-      params["ingredient_compositions"] = ingredientCompositions;
-    }
 
     return fetch("/api/v1/supplements/", {
       method: "POST",
@@ -93,100 +29,14 @@ export class AddSupplementView extends Component {
         return response.json();
       })
       .then(function(responseData) {
-        return responseData;
+        window.location.reload();
       });
-  }
+  };
 
-  createIngredientComposition(
-    ingredientName,
-    ingredientQuantity,
-    ingredientMeasurement
-  ) {
-    // Create or get an ingredient named off of the name input
-    const ingredient = this.createIngredient(ingredientName);
-
-    // ingredient comes back as a promise, so use a then to do what we want
-    const ingredientComposition = ingredient.then(
-      function(ingredientResult) {
-        return this.postIngredientComposition(
-          ingredientResult,
-          ingredientQuantity,
-          ingredientMeasurement
-        );
-      }.bind(this)
-    );
-
-    return ingredientComposition;
-  }
-
-  createIngredientComposition1() {
-    // Embarrassing function, but create an ingredient if inputs are passed
-    if (this.ingredientName1.value && this.ingredientQuantity1.value) {
-      const ingredientName = this.ingredientName1.value;
-      const ingredientQuantity = this.ingredientQuantity1.value;
-      const ingredientMeasurement = this.ingredientMeasurement1;
-
-      return this.createIngredientComposition(
-        ingredientName,
-        ingredientQuantity,
-        ingredientMeasurement
-      ).then();
-    }
-  }
-
-  createIngredientComposition2() {
-    // Embarrassing function, but create an ingredient if inputs are passed
-    if (this.ingredientName2.value && this.ingredientQuantity2.value) {
-      const ingredientName = this.ingredientName2.value;
-      const ingredientQuantity = this.ingredientQuantity2.value;
-      const ingredientMeasurement = this.ingredientMeasurement2;
-
-      return this.createIngredientComposition(
-        ingredientName,
-        ingredientQuantity,
-        ingredientMeasurement
-      );
-    }
-  }
-
-  createIngredientComposition3() {
-    // Embarrassing function, but create an ingredient if inputs are passed
-    if (this.ingredientName3.value && this.ingredientQuantity3.value) {
-      const ingredientName = this.ingredientName3.value;
-      const ingredientQuantity = this.ingredientQuantity3.value;
-      const ingredientMeasurement = this.ingredientMeasurement3;
-
-      return this.createIngredientComposition(
-        ingredientName,
-        ingredientQuantity,
-        ingredientMeasurement
-      );
-    }
-  }
-
-  addSupplementFormData(e) {
+  addSupplementFormData = e => {
     e.preventDefault();
-    const ingredientComposition1 = this.createIngredientComposition1();
-    const ingredientComposition2 = this.createIngredientComposition2();
-    const ingredientComposition3 = this.createIngredientComposition3();
-    Promise.all([
-      ingredientComposition1,
-      ingredientComposition2,
-      ingredientComposition3
-    ]).then(result => {
-      // Not all ingredients may have been entered
-      const validCompositions = result.filter(function(element) {
-        return element !== undefined;
-      });
-      const validCompositionsUUIDs = validCompositions.map(function(obj) {
-        return { uuid: obj.uuid };
-      });
-      // In case there are duplicates, try not to mess up the data
-      const uniqueCompositionUUIDs = [...new Set(validCompositionsUUIDs)];
-
-      this.createSupplement(this.supplementName.value, uniqueCompositionUUIDs);
-    });
-  }
+    this.createSupplement(this.supplementName.value);
+  };
 
   getPossibleSupplements() {
     fetch("/api/v1/measurements/", {
@@ -202,54 +52,12 @@ export class AddSupplementView extends Component {
       });
   }
 
-  renderMeasurements() {
-    const measurementKeys = Object.keys(this.state.measurements);
-
-    return (
-      <div className="form-group col-sm-4">
-        <label><strong>Measurement</strong></label>
-        <select
-          className="form-control"
-          ref={input => this.ingredientMeasurement1 = input}
-        >
-          {measurementKeys.map(key => (
-            <option value={key} key={key}>
-              {this.state.measurements[key].name}
-            </option>
-          ))}
-        </select>
-
-        <select
-          className="form-control"
-          ref={input => this.ingredientMeasurement2 = input}
-        >
-          {measurementKeys.map(key => (
-            <option value={key} key={key}>
-              {this.state.measurements[key].name}
-            </option>
-          ))}
-        </select>
-        <select
-          className="form-control"
-          ref={input => this.ingredientMeasurement3 = input}
-        >
-          {measurementKeys.map(key => (
-            <option value={key} key={key}>
-              {this.state.measurements[key].name}
-            </option>
-          ))}
-        </select>
-      </div>
-    );
-  }
-
   render() {
     return (
       <div className="card">
         <div className="card-header">
           <strong>Create Supplement</strong> (Per Serving)
         </div>
-
         <div className="card-block">
           <form onSubmit={e => this.addSupplementFormData(e)}>
             <div className="row">
@@ -261,56 +69,10 @@ export class AddSupplementView extends Component {
                   placeholder="Black Tea"
                   ref={input => this.supplementName = input}
                 />
+
               </div>
             </div>
-            <div className="row">
-              <div className="form-group col-sm-4">
-                <label><strong>Ingredients</strong><sup>1</sup></label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Caffeine"
-                  ref={input => this.ingredientName1 = input}
-                />
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Theanine"
-                  ref={input => this.ingredientName2 = input}
-                />
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Ginseng"
-                  ref={input => this.ingredientName3 = input}
-                />
-              </div>
-              <div className="form-group col-sm-4">
-                <label><strong>Quantity</strong><sup>2</sup></label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="50"
-                  ref={input => this.ingredientQuantity1 = input}
-                />
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="75"
-                  ref={input => this.ingredientQuantity2 = input}
-                />
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="10"
-                  ref={input => this.ingredientQuantity3 = input}
-                />
-              </div>
-
-              {this.state.ready ? this.renderMeasurements() : ""}
-
-            </div>
-            <div className="float-right">
+            <span className="float-right">
               <button
                 type="submit"
                 id="add-new-object-button"
@@ -319,12 +81,8 @@ export class AddSupplementView extends Component {
               >
                 <i className="fa fa-dot-circle-o" /> Add Supplement
               </button>
-            </div>
+            </span>
           </form>
-          <div>
-            <sup>1) Ingredients are optional inputs. </sup>
-            <sup>2) Per unit of measurement.</sup>
-          </div>
         </div>
       </div>
     );
