042c08714e06c2c8f521208a1a5f3679e70182ff
jeffshek <jeffshek@gmail.com>
2017-09-19 00:30:59 -0400

    Simplify, simplify, simple

--- assets/js/supplements/constants.js
+++ assets/js/supplements/constants.js
@@ -2,37 +2,12 @@ import React from "react";
 import moment from "moment";
 import { Link } from "react-router-dom";
 import { getSupplementOverviewURLFromUUID } from "../routing/routing_utils";
-
-const getIngredientsCompositionsLabels = ingredient_compositions => {
-  let ingredientLabels = [];
-  // You should probably switch to a map function here ...
-  for (let i = 0; i < ingredient_compositions.length; i++) {
-    let details = ingredient_compositions[i];
-    let ingredient_name = details.ingredient.name;
-    let measurement_size;
-
-    try {
-      measurement_size = details.measurement.short_name;
-    } catch (err) {
-      measurement_size = "";
-    }
-
-    let quantity = details.quantity || "1";
-
-    // Describe it as 150 mg Caffeine
-    let formatted_label = `${quantity}${measurement_size} ${ingredient_name}`;
-
-    ingredientLabels.push(formatted_label);
-  }
-
-  return ingredientLabels.join(", ");
-};
+import { DATETIME_CREATED_FORMAT } from "../constants/dates_and_times";
 
 export const SupplementHistoryTableHeader = () => (
   <thead>
     <tr>
       <th>Name</th>
-      <th className="center-source">Ingredients</th>
       <th className="center-source">Actions</th>
       <th>Date Added</th>
     </tr>
@@ -44,16 +19,12 @@ export const SupplementRow = props => {
 
   const { uuid, name } = data;
   const dateCreated = data.created;
-  const ingredientsFormatted = getIngredientsCompositionsLabels(
-    data.ingredient_compositions
-  );
-  const timeFormatted = moment(dateCreated).format("l - h:mm:ss a");
+  const timeFormatted = moment(dateCreated).format(DATETIME_CREATED_FORMAT);
   const supplementOverviewLink = getSupplementOverviewURLFromUUID(uuid);
 
   return (
     <tr>
       <td><Link to={supplementOverviewLink}>{name}</Link></td>
-      <td>{ingredientsFormatted}</td>
       <td>
         <div className="center-icon">
           <div className="edit-icon" onClick={e => props.selectModalEdit(data)}>
--- assets/js/supplements/supplements_view.js
+++ assets/js/supplements/supplements_view.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component } from "react";
 import { SupplementTable } from "./supplements_table";
 import { AddSupplementView } from "./add_supplement";
 
