dce5a117d2fdfed244095539336da6c7b000b8da
jeffshek <jeffshek@gmail.com>
2017-10-04 22:58:39 -0400

    Don't need state, make it wait

--- assets/js/supplement_reminders/supplement_reminders_view.js
+++ assets/js/supplement_reminders/supplement_reminders_view.js
@@ -1,23 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import { AddSupplementReminderView } from "./add_supplement_reminder";
 import { SupplementReminderTable } from "./supplement_reminder_table";
 
-export class SupplementRemindersView extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="card-header">
-          <strong id="add-supplement-entry-text">
-            Add Texting Reminders (Limit 5 A Day)
-          </strong>
-        </div>
-        <AddSupplementReminderView />
-        <SupplementReminderTable />
+export const SupplementRemindersView = () => {
+  return (
+    <div>
+      <div className="card-header">
+        <strong id="add-supplement-entry-text">
+          Add Texting Reminders (Limit 5 A Day)
+        </strong>
       </div>
-    );
-  }
-}
+      <AddSupplementReminderView />
+      <SupplementReminderTable />
+    </div>
+  );
+};
