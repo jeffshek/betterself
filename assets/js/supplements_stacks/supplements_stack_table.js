ec633e5407fc6d721f822d05e0c50db95a2b6888
Jeffrey Shek <jeffshek@gmail.com>
2017-11-05 02:14:59 -0500

    More frontend work to support editing of supplement stacks

--- assets/js/supplements_stacks/supplements_stack_table.js
+++ assets/js/supplements_stacks/supplements_stack_table.js
@@ -2,16 +2,20 @@ import React from "react";
 import { CubeLoadingStyle } from "../constants/loading_styles";
 import { BaseLogTable } from "../resources_table/resource_table";
 import { SupplementStackRow, SupplementStackTableHeader } from "./constants";
-import { JSON_AUTHORIZATION_HEADERS } from "../constants/requests";
 import { getFetchJSONAPI } from "../utils/fetch_utils";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import Select from "react-select";
 
 export class SupplementStackTable extends BaseLogTable {
   constructor() {
     super();
 
     this.state = {
+      supplements: [],
       supplementsStacks: [],
-      renderReady: false
+      renderReady: false,
+      addModal: true,
+      selectedSupplementQuantity: 1
     };
 
     this.resourceURL = "/api/v1/supplements_stacks/";
@@ -19,6 +23,7 @@ export class SupplementStackTable extends BaseLogTable {
 
   componentDidMount() {
     this.getSupplementStacks();
+    this.getSupplements();
   }
 
   getSupplementStacks() {
@@ -58,6 +63,100 @@ export class SupplementStackTable extends BaseLogTable {
     );
   }
 
+  submitEdit() {
+    const params = {
+      uuid: this.state.editObject["uuid"],
+      name: this.state["supplementName"]
+    };
+
+    this.putParamsUpdate(params);
+    this.toggle();
+  }
+
+  getSupplements() {
+    const url = "/api/v1/supplements/";
+    getFetchJSONAPI(url).then(responseData => {
+      this.setState({
+        supplements: responseData
+      });
+    });
+  }
+
+  handleSupplementChange = val => {
+    let updatedLocation;
+    if (val) {
+      updatedLocation = val.value;
+    } else {
+      updatedLocation = null;
+    }
+    this.setState({
+      selectedSupplementIndex: updatedLocation
+    });
+  };
+
+  handleSettingsChange = event => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    const intValue = parseInt(value);
+
+    this.setState({
+      [name]: intValue
+    });
+  };
+
+  renderAddModal() {
+    if (!this.state.addModal) {
+      return <div />;
+    }
+
+    if (!this.state.supplements) {
+      return <div />;
+    }
+
+    // React-Select needs it in this value: label format
+    const supplementsKeys = Object.keys(this.state.supplements);
+    const supplementDetails = supplementsKeys.map(e => {
+      return {
+        value: e,
+        label: this.state.supplements[e].name
+      };
+    });
+
+    return (
+      <Modal isOpen={this.state.addModal} toggle={this.toggle}>
+        <ModalHeader toggle={this.toggle}>Add Supplement to Stack</ModalHeader>
+        <ModalBody>
+          <label className="form-control-label add-event-label">
+            Supplement Name
+          </label>
+          <Select
+            name="form-field-name"
+            value={this.state.selectedSupplementIndex}
+            options={supplementDetails}
+            onChange={this.handleSupplementChange}
+          />
+          <br />
+          <label className="form-control-label add-event-label">
+            Supplement Quantity
+          </label>
+          <input
+            name="updateRollingWindow"
+            type="number"
+            className="form-control"
+            defaultValue={this.state.selectedSupplementQuantity}
+            onChange={this.handleSettingsChange}
+          />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="decline-modal" onClick={this.toggle}>Cancel</Button>
+          <Button color="primary" onClick={this.submitEdit}>Update</Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+
   render() {
     return (
       <div className="card">
@@ -66,6 +165,7 @@ export class SupplementStackTable extends BaseLogTable {
           <strong>Supplement Stacks</strong>
         </div>
         {this.renderReady()}
+        {this.renderAddModal()}
       </div>
     );
   }
