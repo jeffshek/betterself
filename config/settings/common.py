14d494a16ce46bcaf71689a661eda18d7ddcc857
Jeffrey Shek <jeffshek@gmail.com>
2017-11-19 00:04:16 -0500

    Refactor Login to use 4 spaces instead of 2

--- config/settings/common.py
+++ config/settings/common.py
@@ -227,7 +227,6 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = 'users:redirect'
 LOGIN_URL = 'account_login'
 
-# SLUGLIFIER
 AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
 
 # Location of root django.contrib.admin URL, use {% url 'admin:index' %}
